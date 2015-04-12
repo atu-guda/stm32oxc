0179b189aed696766e9ccbb741c4ff8674fb9378
Anton Guda <atu@nmetau.edu.ua>
2015-04-13 00:00:10 +0300

    Add EXTI handling to example 02

--- examples/f407_x2/02_leds_rtos/main.cpp
+++ examples/f407_x2/02_leds_rtos/main.cpp
@@ -5,7 +5,7 @@
 #include <task.h>
 #include <queue.h>
 
-// void MX_GPIO_Init(void);
+void MX_GPIO_Init(void);
 
 
 // PinsOut p1 { GPIOC, 0, 4 };
@@ -15,9 +15,15 @@ const int def_stksz = 2 * configMINIMAL_STACK_SIZE;
 
 extern "C" {
 
+void EXTI0_IRQHandler(void);
+void EXTI1_IRQHandler(void);
+void HAL_GPIO_EXTI_Callback( uint16_t pin );
+
 void task_leds( void *prm UNUSED_ARG );
 
 }
+
+volatile int led_delay = 1000;
 
 int main(void)
 {
@@ -25,6 +31,7 @@ int main(void)
 
   SystemClock_Config();
   leds.initHW();
+  MX_GPIO_Init();
 
   leds.write( 0x0F );  delay_bad_ms( 200 );
   leds.write( 0x0A );  delay_bad_ms( 200 );
@@ -47,7 +54,7 @@ void task_leds( void *prm UNUSED_ARG )
     leds.write( i );
     ++i;
     i &= 0x0F;
-    delay_ms( 1000 );
+    delay_ms( led_delay );
     // HAL_Delay( 1000 );
   }
 }
@@ -59,19 +66,52 @@ void _exit( int rc )
 }
 
 // // configs
-// void MX_GPIO_Init(void)
-// {
-//
-//   // GPIO_InitTypeDef GPIO_InitStruct;
-//   // moved to PinsOut initHW
-//
-//   /*Configure GPIO pins : PA0 PA1 */
-//   // GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1;
-//   // GPIO_InitStruct.Mode = GPIO_MODE_EVT_RISING;
-//   // GPIO_InitStruct.Pull = GPIO_PULLDOWN;
-//   // HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-//
-// }
+void MX_GPIO_Init(void)
+{
+  // putput init moved to PinsOut initHW
+
+  __HAL_RCC_SYSCFG_CLK_ENABLE();
+  __GPIOA_CLK_ENABLE();
+  GPIO_InitTypeDef gpi;
+
+  /* Configure  input GPIO pins : PA0 PA1 */
+  gpi.Pin = GPIO_PIN_0 | GPIO_PIN_1;
+  // gpi.Mode = GPIO_MODE_EVT_RISING;
+  gpi.Mode = GPIO_MODE_IT_RISING;
+  gpi.Pull = GPIO_PULLDOWN;
+  HAL_GPIO_Init( GPIOA, &gpi );
+
+  HAL_NVIC_SetPriority( EXTI0_IRQn, configKERNEL_INTERRUPT_PRIORITY, 0 );
+  HAL_NVIC_SetPriority( EXTI1_IRQn, configKERNEL_INTERRUPT_PRIORITY, 0 );
+  // HAL_NVIC_SetPriority( EXTI0_IRQn, 4, 0 );
+  // HAL_NVIC_SetPriority( EXTI1_IRQn, 4, 0 );
+  HAL_NVIC_EnableIRQ( EXTI0_IRQn );
+  HAL_NVIC_EnableIRQ( EXTI1_IRQn );
+}
+
+void EXTI0_IRQHandler(void)
+{
+  HAL_GPIO_EXTI_IRQHandler( BIT0 );
+}
+
+void EXTI1_IRQHandler(void)
+{
+  HAL_GPIO_EXTI_IRQHandler( BIT1 );
+}
+
+void HAL_GPIO_EXTI_Callback( uint16_t pin )
+{
+  if( pin == BIT0 )  {
+    leds.set( 0x0F );
+    led_delay = 1000;
+  }
+  if( pin == BIT1 )  {
+    leds.reset( 0x0F );
+    led_delay >>= 1;
+    ++led_delay;
+  }
+  leds.toggle( 0x01 );
+}
 
 FreeRTOS_to_stm32cube_tick_hook;
 
