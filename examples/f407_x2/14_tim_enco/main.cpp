a82254f98f86bdcebfee9653447ac78253978ba2
Anton Guda <atu@nmetau.edu.ua>
2015-05-01 19:38:35 +0300

    Add encoder example

---
+++ examples/f407_x2/14_tim_enco/main.cpp
@@ -0,0 +1,270 @@
+#include <cstring>
+#include <cstdlib>
+
+
+#include <bsp/board_stm32f407_atu_x2.h>
+#include <oxc_gpio.h>
+#include <oxc_usbcdcio.h>
+#include <oxc_console.h>
+#include <oxc_debug1.h>
+#include <oxc_smallrl.h>
+
+#include "usbd_desc.h"
+#include <usbd_cdc.h>
+#include <usbd_cdc_interface.h>
+
+#include <FreeRTOS.h>
+#include <task.h>
+#include <queue.h>
+
+using namespace std;
+using namespace SMLRL;
+
+// PinsOut p1 { GPIOC, 0, 4 };
+BOARD_DEFINE_LEDS;
+
+UsbcdcIO usbcdc;
+
+TIM_HandleTypeDef tim1h;
+TIM_Encoder_InitTypeDef enco_cfg;
+void init_enco();
+
+const int def_stksz = 2 * configMINIMAL_STACK_SIZE;
+
+// SmallRL storage and config
+int smallrl_print( const char *s, int l );
+int smallrl_exec( const char *s, int l );
+void smallrl_sigint(void);
+
+
+SmallRL srl( smallrl_print, smallrl_exec );
+// SmallRL srl( smallrl_print, exec_queue );
+
+// --- local commands;
+int cmd_test0( int argc, const char * const * argv );
+CmdInfo CMDINFO_TEST0 { "test0", 'T', cmd_test0, " - test something 0"  };
+
+int idle_flag = 0;
+int break_flag = 0;
+
+
+const CmdInfo* global_cmds[] = {
+  DEBUG_CMDS,
+
+  &CMDINFO_TEST0,
+  nullptr
+};
+
+
+extern "C" {
+
+void task_main( void *prm UNUSED_ARG );
+void task_leds( void *prm UNUSED_ARG );
+void task_gchar( void *prm UNUSED_ARG );
+
+
+}
+
+STD_USBCDC_SEND_TASK( usbcdc );
+
+int main(void)
+{
+  HAL_Init();
+
+  SystemClock_Config();
+  leds.initHW();
+
+  leds.write( 0x0F );  delay_bad_ms( 200 );
+  leds.write( 0x00 );
+
+  global_smallrl = &srl;
+
+  user_vars['t'-'a'] = 10;
+  user_vars['n'-'a'] = 2000;
+
+
+  //           code               name    stack_sz      param  prty TaskHandle_t*
+  xTaskCreate( task_leds,        "leds", 1*def_stksz, nullptr,   1, nullptr );
+  xTaskCreate( task_usbcdc_send, "send", 2*def_stksz, nullptr,   2, nullptr );  // 2
+  xTaskCreate( task_main,        "main", 2*def_stksz, nullptr,   1, nullptr );
+  xTaskCreate( task_gchar,      "gchar", 2*def_stksz, nullptr,   2, nullptr ); // 1?
+
+  vTaskStartScheduler();
+  die4led( 0xFF );
+
+
+
+  return 0;
+}
+
+void task_leds( void *prm UNUSED_ARG )
+{
+  while (1)
+  {
+    leds.toggle( BIT1 );
+    delay_ms( 500 );
+  }
+}
+
+void task_main( void *prm UNUSED_ARG ) // TMAIN
+{
+  uint32_t nl = 0;
+
+  usbcdc.init();
+  delay_ms( 50 );
+
+  init_enco();
+
+  delay_ms( 10 );
+  pr( "*=*** Main loop: ****** " NL );
+
+  srl.setSigFun( smallrl_sigint );
+  srl.set_ps1( "\033[32m#\033[0m ", 2 );
+  srl.re_ps();
+  srl.set_print_cmd( true );
+
+
+  idle_flag = 1;
+  while(1) {
+    ++nl;
+    if( idle_flag == 0 ) {
+      pr_sd( ".. main idle  ", nl );
+      srl.redraw();
+    }
+    idle_flag = 0;
+    delay_ms( 60000 );
+    // delay_ms( 1 );
+
+  }
+  vTaskDelete(NULL);
+}
+
+void task_gchar( void *prm UNUSED_ARG )
+{
+  char sc[2] = { 0, 0 };
+  while (1) {
+    int n = usbcdc.recvByte( sc, 10000 );
+    if( n ) {
+      srl.addChar( sc[0] );
+      idle_flag = 1;
+    }
+  }
+  vTaskDelete(NULL);
+}
+
+
+void _exit( int rc )
+{
+  exit_rc = rc;
+  die4led( rc );
+}
+
+
+int pr( const char *s )
+{
+  if( !s || !*s ) {
+    return 0;
+  }
+  prl( s, strlen(s) );
+  return 0;
+}
+
+int prl( const char *s, int l )
+{
+  // usbcdc.sendBlockSync( s, l );
+  usbcdc.sendBlock( s, l );
+  idle_flag = 1;
+  return 0;
+}
+
+// ---------------------------- smallrl -----------------------
+
+
+int smallrl_print( const char *s, int l )
+{
+  prl( s, l );
+  return 1;
+}
+
+int smallrl_exec( const char *s, int l )
+{
+  // pr( NL "Cmd: \"" );
+  // prl( s, l );
+  // pr( "\" " NL );
+  exec_direct( s, l );
+  return 1;
+}
+
+
+void smallrl_sigint(void)
+{
+  break_flag = 1;
+  idle_flag = 1;
+  leds.toggle( BIT3 );
+}
+
+// TEST0
+int cmd_test0( int argc, const char * const * argv )
+{
+  int n = user_vars['n'-'a'];
+  int t = user_vars['t'-'a'];
+  pr( NL "Test0: n= " ); pr_d( n ); pr( "  t= " ); pr_d( t ); pr( NL );
+
+  delay_ms( 10 );
+  break_flag = 0;
+
+
+  int old_cnt = -1;
+  for( int i=0; i<n && !break_flag; ++i ) {
+    uint16_t t_cnt = tim1h.Instance->CNT;
+    // uint16_t t_ccr1 = tim1h.Instance->CCR1;
+    if( t_cnt != old_cnt ) {
+      pr( "[" ); pr_d( i );
+      pr( "]  CNT= " ); pr_d( t_cnt );
+      pr( "  D= " ); pr_d( t_cnt - old_cnt );
+      // pr( "  CCR1= " ); pr_d( t_ccr1 );
+      pr( NL );
+    }
+    old_cnt = t_cnt;
+    delay_ms( t );
+  }
+
+  pr( NL "test0 end." NL );
+  return 0;
+}
+
+//  ----------------------------- configs ----------------
+//
+void init_enco()
+{
+  tim1h.Instance = TIM1;
+  tim1h.Init.Period            = 0xFFFF;
+  tim1h.Init.Prescaler         = 0; // 0?
+  tim1h.Init.ClockDivision     = 0;
+  tim1h.Init.CounterMode       = TIM_COUNTERMODE_UP;
+  tim1h.Init.RepetitionCounter = 0;
+
+  enco_cfg.EncoderMode    = TIM_ENCODERMODE_TI12;
+  enco_cfg.IC1Polarity    = TIM_ICPOLARITY_RISING;
+  enco_cfg.IC1Selection   = TIM_ICSELECTION_DIRECTTI;
+  enco_cfg.IC1Prescaler   = TIM_ICPSC_DIV1;
+  enco_cfg.IC1Filter      = 0x0F; /// 0-F
+  enco_cfg.IC2Polarity    = TIM_ICPOLARITY_RISING;
+  enco_cfg.IC2Selection   = TIM_ICSELECTION_DIRECTTI;
+  enco_cfg.IC2Prescaler   = TIM_ICPSC_DIV1;
+  enco_cfg.IC2Filter      = 0x0F; /// 0-F
+
+  if( HAL_TIM_Encoder_Init( &tim1h, &enco_cfg ) != HAL_OK ) {
+    user_vars['e'-'a'] = 1;
+    return;
+  }
+  HAL_TIM_Encoder_Start( &tim1h, TIM_CHANNEL_ALL );
+}
+
+
+
+
+FreeRTOS_to_stm32cube_tick_hook;
+
+// vim: path=.,/usr/share/stm32lib/inc/,/usr/arm-none-eabi/include,../../../inc
+
---
+++ examples/f407_x2/14_tim_enco/stm32f4xx_hal_msp.cpp
@@ -0,0 +1,60 @@
+/**
+  * File Name          : stm32f4xx_hal_msp.c
+  * Description        : This file provides code for the MSP Initialization
+  *                      and de-Initialization codes.
+  ******************************************************************************
+  */
+
+// #include <stm32f4xx_hal.h>
+// #include <usbd_core.h>
+
+#include <oxc_base.h>
+
+void default_USBFS_MspInit(void);
+
+void HAL_PCD_MspInit( PCD_HandleTypeDef *hpcd UNUSED_ARG )
+{
+  default_USBFS_MspInit();
+}
+
+void HAL_TIM_Encoder_MspInit( TIM_HandleTypeDef* timh_enco )
+{
+  if( timh_enco->Instance != TIM1 ) {
+    return;
+  }
+  __TIM1_CLK_ENABLE();
+  __GPIOE_CLK_ENABLE();
+
+  GPIO_InitTypeDef gpi;
+
+  /**TIM1 GPIO Configuration
+    PE9      ------> TIM1_CH1
+    PE11     ------> TIM1_CH2
+    */
+  gpi.Pin = GPIO_PIN_9 | GPIO_PIN_11;
+  gpi.Mode = GPIO_MODE_AF_PP;
+  // gpi.Pull = GPIO_PULLDOWN;
+  gpi.Pull = GPIO_NOPULL; // use external resistors
+  gpi.Speed = GPIO_SPEED_LOW;
+  gpi.Alternate = GPIO_AF1_TIM1;
+  HAL_GPIO_Init( GPIOE, &gpi );
+
+  /* Peripheral interrupt init*/
+  /* Sets the priority grouping field */
+  // HAL_NVIC_SetPriority( TIM1_BRK_TIM9_IRQn, 14, 0 );
+  // HAL_NVIC_EnableIRQ( TIM1_BRK_TIM9_IRQn );
+
+}
+
+void HAL_TIM_PWM_MspDeInit( TIM_HandleTypeDef* timh_enco )
+{
+  if( timh_enco->Instance !=TIM1 ) {
+    return;
+  }
+  __TIM1_CLK_DISABLE();
+  HAL_GPIO_DeInit( GPIOE, GPIO_PIN_9|GPIO_PIN_11 );
+
+  /* Peripheral interrupt Deinit*/
+  // HAL_NVIC_DisableIRQ(TIM1_BRK_TIM9_IRQn);
+}
+
