47a8b6477a12d04f3cd193e79c5898abec25b3e6
Anton Guda <atu@nmetau.edu.ua>
2015-05-10 14:55:31 +0300

    Add stepmotor example 16

---
+++ examples/f407_x2/16_step1/main.cpp
@@ -0,0 +1,292 @@
+#include <cstring>
+#include <cstdlib>
+
+
+#include <bsp/board_stm32f407_atu_x2.h>
+#include <oxc_gpio.h>
+#include <oxc_usbcdcio.h>
+#include <oxc_console.h>
+#include <oxc_debug1.h>
+#include <oxc_smallrl.h>
+
+#include "usbd_desc.h"
+#include <usbd_cdc.h>
+#include <usbd_cdc_interface.h>
+
+#include <FreeRTOS.h>
+#include <task.h>
+#include <queue.h>
+
+using namespace std;
+using namespace SMLRL;
+
+// PinsOut p1 { GPIOC, 0, 4 };
+BOARD_DEFINE_LEDS;
+
+UsbcdcIO usbcdc;
+
+
+const int def_stksz = 2 * configMINIMAL_STACK_SIZE;
+
+// SmallRL storage and config
+int smallrl_print( const char *s, int l );
+int smallrl_exec( const char *s, int l );
+void smallrl_sigint(void); // unused?
+
+
+SmallRL srl( smallrl_print, smallrl_exec );
+// SmallRL srl( smallrl_print, exec_queue );
+
+// --- local commands;
+int cmd_test0( int argc, const char * const * argv );
+CmdInfo CMDINFO_TEST0 { "test0", 'T', cmd_test0, " [N] [01] - test step 0"  };
+
+int idle_flag = 0;
+int break_flag = 0;
+
+
+const CmdInfo* global_cmds[] = {
+  DEBUG_CMDS,
+
+  &CMDINFO_TEST0,
+  nullptr
+};
+
+PinsOut motor { GPIOE, 0, 4 };
+
+const uint8_t half_steps4[] = { 1, 3, 2, 6, 4, 12, 8, 9 };
+const uint8_t full_steps4[] = { 1, 2, 4, 8 };
+const uint8_t half_steps3[] = { 1, 3, 2, 6, 4, 5 };
+
+struct MotorMode {
+  int n_steps;
+  const uint8_t *steps;
+};
+
+MotorMode m_modes[] = {
+  { 8, half_steps4 },
+  { 4, full_steps4 },
+  { 6, half_steps3 }, // 3-phase modes
+  { 3, full_steps4 }  // part of 4-phase
+};
+const int n_modes = sizeof(m_modes)/sizeof(MotorMode);
+
+
+extern "C" {
+
+void task_main( void *prm UNUSED_ARG );
+void task_leds( void *prm UNUSED_ARG );
+void task_gchar( void *prm UNUSED_ARG );
+
+}
+
+STD_USBCDC_SEND_TASK( usbcdc );
+
+int main(void)
+{
+  HAL_Init();
+
+  SystemClock_Config();
+  leds.initHW();
+
+  motor.initHW();
+
+  leds.write( 0x0F );  delay_bad_ms( 200 );
+
+  //usbcdc.init();
+  // leds.write( 0x07 );  delay_bad_ms( 200 );
+
+
+  leds.write( 0x00 );
+
+  user_vars['t'-'a'] = 1000;
+  user_vars['n'-'a'] = 10;
+
+  global_smallrl = &srl;
+
+  //           code               name    stack_sz      param  prty TaskHandle_t*
+  xTaskCreate( task_leds,        "leds", 1*def_stksz, nullptr,   1, nullptr );
+  xTaskCreate( task_usbcdc_send, "send", 2*def_stksz, nullptr,   2, nullptr );  // 2
+  xTaskCreate( task_main,        "main", 2*def_stksz, nullptr,   1, nullptr );
+  xTaskCreate( task_gchar,      "gchar", 2*def_stksz, nullptr,   1, nullptr );
+
+  vTaskStartScheduler();
+  die4led( 0xFF );
+
+
+
+  return 0;
+}
+
+void task_leds( void *prm UNUSED_ARG )
+{
+  while (1)
+  {
+    leds.toggle( BIT1 );
+    delay_ms( 500 );
+  }
+}
+
+void task_main( void *prm UNUSED_ARG ) // TMAIN
+{
+  uint32_t nl = 0;
+
+  usbcdc.init();
+  delay_ms( 50 );
+
+  delay_ms( 10 );
+  pr( "*=*** Main loop: ****** " NL );
+
+  srl.setSigFun( smallrl_sigint );
+  srl.set_ps1( "\033[32m#\033[0m ", 2 );
+  srl.re_ps();
+  srl.set_print_cmd( true );
+
+
+  idle_flag = 1;
+  while(1) {
+    ++nl;
+    if( idle_flag == 0 ) {
+      pr_sd( ".. main idle  ", nl );
+      srl.redraw();
+    }
+    idle_flag = 0;
+    delay_ms( 60000 );
+    // delay_ms( 1 );
+
+  }
+  vTaskDelete(NULL);
+}
+
+void task_gchar( void *prm UNUSED_ARG )
+{
+  char sc[2] = { 0, 0 };
+  while (1) {
+    int n = usbcdc.recvByte( sc, 10000 );
+    if( n ) {
+      srl.addChar( sc[0] );
+      idle_flag = 1;
+    }
+  }
+  vTaskDelete(NULL);
+}
+
+
+
+
+void _exit( int rc )
+{
+  exit_rc = rc;
+  die4led( rc );
+}
+
+
+int pr( const char *s )
+{
+  if( !s || !*s ) {
+    return 0;
+  }
+  prl( s, strlen(s) );
+  return 0;
+}
+
+int prl( const char *s, int l )
+{
+  usbcdc.sendBlock( s, l );
+  idle_flag = 1;
+  return 0;
+}
+
+// ---------------------------- smallrl -----------------------
+
+
+int smallrl_print( const char *s, int l )
+{
+  prl( s, l );
+  return 1;
+}
+
+int smallrl_exec( const char *s, int l )
+{
+  exec_direct( s, l );
+  return 1;
+}
+
+
+void smallrl_sigint(void)
+{
+  break_flag = 1;
+  idle_flag = 1;
+  leds.toggle( BIT3 );
+}
+
+// TEST0
+int cmd_test0( int argc, const char * const * argv )
+{
+  static int ph = 0;
+  int n = user_vars['n'-'a'];
+  uint32_t t_step = user_vars['t'-'a'];
+  if( argc > 1 ) {
+    n = strtol( argv[1], 0, 0 );
+  }
+
+  int m = user_vars['m'-'a'];
+  if( m >= n_modes ) {
+    m = 0;
+  }
+
+  const uint8_t *steps = m_modes[m].steps;
+  int ns = m_modes[m].n_steps;
+  if( ph >= ns ||  ph < 0 ) { ph = 0; }
+
+  int d = 1;
+  if( argc > 2  && argv[2][0] == '-' ) {
+    d = ns - 1; // % no zero
+  }
+  pr( NL "Test0: n= " ); pr_d( n ); pr( " t= " ); pr_d( t_step );
+  pr( " m= " ); pr_d( m );
+  pr( " d= " ); pr_d( d );
+  pr( NL );
+
+  if( n < 1 ) {
+    motor.write( 0 );
+    ph = 0;
+    pr( NL "Motor off." NL );
+    return 0;
+  }
+
+
+  TickType_t tc0 = xTaskGetTickCount(), tc00 = tc0;
+
+  break_flag = 0;
+  for( int i=0; i<n && !break_flag; ++i ) {
+    TickType_t tcc = xTaskGetTickCount();
+    if( t_step > 500 ) {
+      pr( " Step  i= " ); pr_d( i );
+      pr( " ph: " ); pr_d( ph );
+      pr( " v: " ); pr_h( steps[ph] );
+      pr( "  tick: "); pr_d( tcc - tc00 );
+      pr( NL );
+    }
+    motor.write( steps[ph] );
+    ph += d;
+    ph %= ns;
+    vTaskDelayUntil( &tc0, t_step );
+    // delay_ms( t_step );
+  }
+
+  pr( NL );
+
+  delay_ms( 10 );
+  break_flag = 0;
+
+  pr( NL "test0 end." NL );
+  return 0;
+}
+
+//  ----------------------------- configs ----------------
+
+FreeRTOS_to_stm32cube_tick_hook;
+
+// vim: path=.,/usr/share/stm32lib/inc/,/usr/arm-none-eabi/include,../../../inc
+
