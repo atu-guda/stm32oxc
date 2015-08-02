9dfffb476b1cf81e1b9356f804cd084aaadfaa4a
Anton Guda <atu@nmetau.edu.ua>
2015-08-02 22:21:38 +0300

    Add DS3231 RTC example for f3disco

---
+++ examples/f303dis/11_i2c_ds3231/main.cpp
@@ -0,0 +1,226 @@
+#include <cstring>
+#include <cstdlib>
+
+#include <oxc_gpio.h>
+#include <oxc_usartio.h>
+#include <oxc_console.h>
+#include <oxc_debug1.h>
+#include <oxc_debug_i2c.h>
+#include <oxc_ds3231.h>
+#include <oxc_common1.h>
+#include <oxc_smallrl.h>
+
+#include <FreeRTOS.h>
+#include <task.h>
+#include <queue.h>
+
+using namespace std;
+using namespace SMLRL;
+
+// PinsOut p1 { GPIOE, 8, 8 };
+BOARD_DEFINE_LEDS;
+
+
+
+const int def_stksz = 1 * configMINIMAL_STACK_SIZE;
+
+SmallRL srl( smallrl_print, smallrl_exec );
+
+// --- local commands;
+int cmd_test0( int argc, const char * const * argv );
+CmdInfo CMDINFO_TEST0 { "test0", 'T', cmd_test0, " - test something 0"  };
+
+int cmd_set_time( int argc, const char * const * argv );
+CmdInfo CMDINFO_SET_TIME { "stime", 0, cmd_set_time, " hour min sec - set RTC time "  };
+
+int cmd_set_date( int argc, const char * const * argv );
+CmdInfo CMDINFO_SET_DATE { "sdate", 0, cmd_set_date, " year month day - set RTC date "  };
+
+const CmdInfo* global_cmds[] = {
+  DEBUG_CMDS,
+  DEBUG_I2C_CMDS,
+
+  &CMDINFO_SET_TIME,
+  &CMDINFO_SET_DATE,
+  &CMDINFO_TEST0,
+  nullptr
+};
+
+
+extern "C" {
+void task_main( void *prm UNUSED_ARG );
+}
+
+#define RESET_I2C  __HAL_I2C_DISABLE( &i2ch ); delay_ms( 10 ); __HAL_I2C_ENABLE( &i2ch );  delay_ms( 10 );
+I2C_HandleTypeDef i2ch;
+DS3231 rtc( i2ch );
+void MX_I2C1_Init( I2C_HandleTypeDef &i2c );
+
+UART_HandleTypeDef uah;
+UsartIO usartio( &uah, USART2 );
+int init_uart( UART_HandleTypeDef *uahp, int baud = 115200 );
+
+STD_USART2_SEND_TASK( usartio );
+// STD_USART2_RECV_TASK( usartio );
+STD_USART2_IRQ( usartio );
+
+int main(void)
+{
+  HAL_Init();
+
+  SystemClock_Config();
+  leds.initHW();
+
+  leds.write( 0x0F );  delay_bad_ms( 200 );
+  if( !init_uart( &uah ) ) {
+    die4led( 0x08 );
+  }
+  leds.write( 0x0A );  delay_bad_ms( 200 );
+
+  // HAL_UART_Transmit( &uah, (uint8_t*)"START\r\n", 7, 100 );
+
+  usartio.sendStrSync( "0123456789---main()---ABCDEF" NL );
+
+  MX_I2C1_Init( i2ch );
+  i2ch_dbg = &i2ch;
+
+
+  leds.write( 0x00 );
+
+  user_vars['t'-'a'] = 1000;
+  user_vars['n'-'a'] = 10;
+
+  global_smallrl = &srl;
+
+  //           code               name    stack_sz      param  prty TaskHandle_t*
+  xTaskCreate( task_leds,        "leds", 1*def_stksz, nullptr,   1, nullptr );
+  xTaskCreate( task_usart2_send, "send", 1*def_stksz, nullptr,   2, nullptr );  // 2
+  xTaskCreate( task_main,        "main", 1*def_stksz, nullptr,   1, nullptr );
+  xTaskCreate( task_gchar,      "gchar", 2*def_stksz, nullptr,   1, nullptr );
+
+  vTaskStartScheduler();
+  die4led( 0xFF );
+
+
+
+  return 0;
+}
+
+void task_main( void *prm UNUSED_ARG ) // TMAIN
+{
+  uint32_t nl = 0;
+
+  delay_ms( 50 );
+  user_vars['t'-'a'] = 1000;
+
+  usartio.itEnable( UART_IT_RXNE );
+  usartio.setOnSigInt( sigint );
+  devio_fds[0] = &usartio; // stdin
+  devio_fds[1] = &usartio; // stdout
+  devio_fds[2] = &usartio; // stderr
+
+  delay_ms( 50 );
+  pr( "*=*** Main loop: ****** " NL );
+  delay_ms( 20 );
+
+  srl.setSigFun( smallrl_sigint );
+  srl.set_ps1( "\033[32m#\033[0m ", 2 );
+  srl.re_ps();
+  srl.set_print_cmd( true );
+
+
+  idle_flag = 1;
+  while(1) {
+    ++nl;
+    if( idle_flag == 0 ) {
+      pr_sd( ".. main idle  ", nl );
+      srl.redraw();
+    }
+    idle_flag = 0;
+    delay_ms( 60000 );
+    // delay_ms( 1 );
+
+  }
+  vTaskDelete(NULL);
+}
+
+
+// TEST0
+int cmd_test0( int argc, const char * const * argv )
+{
+  int n = user_vars['n'-'a'];
+  uint32_t t_step = user_vars['t'-'a'];
+  if( argc > 1 ) {
+    n = strtol( argv[1], 0, 0 );
+  }
+  pr( NL "Test0: n= " ); pr_d( n ); pr( " t= " ); pr_d( t_step );
+  pr( NL );
+
+  RESET_I2C;
+  char time_buf[10], date_buf[14];
+  uint8_t t_hour, t_min, t_sec;
+
+  TickType_t tc0 = xTaskGetTickCount();
+
+  break_flag = 0;
+  rtc.setCtl( 0 ); // enable only clock on bat
+
+  for( int i=0; i<n && !break_flag ; ++i ) {
+
+    pr( "[" ); pr_d( i ); pr( "]  " );
+    rtc.getTime( &t_hour, &t_min, &t_sec );
+    rtc.getTimeStr( time_buf );
+    pr( time_buf );
+    pr( "   =   " ); pr_d( t_hour ); pr( ":" ); pr_d( t_min ); pr( ":" ); pr_d( t_sec );
+    rtc.getDateStr( date_buf );
+    pr( "  / " ); pr( date_buf );
+    pr( NL );
+    vTaskDelayUntil( &tc0, t_step );
+    // delay_ms( t_step );
+  }
+
+  pr( NL );
+
+  delay_ms( 10 );
+  break_flag = 0;
+  idle_flag = 1;
+
+  pr( NL "test0 end." NL );
+  return 0;
+}
+
+int cmd_set_time( int argc, const char * const * argv )
+{
+  uint8_t t_hour, t_min, t_sec;
+  if( argc < 4 ) {
+    pr( "3 args required" NL );
+    return 1;
+  }
+  t_hour = atoi( argv[1] );
+  t_min  = atoi( argv[2] );
+  t_sec  = atoi( argv[3] );
+  return rtc.setTime( t_hour, t_min, t_sec );
+}
+
+
+int cmd_set_date( int argc, const char * const * argv )
+{
+  uint16_t year;
+  uint8_t mon, day;
+  if( argc < 4 ) {
+    pr( "3 args required" NL );
+    return 1;
+  }
+  year = atoi( argv[1] );
+  mon  = atoi( argv[2] );
+  day  = atoi( argv[3] );
+  return rtc.setDate( year, mon, day );
+}
+
+
+//  ----------------------------- configs ----------------
+
+FreeRTOS_to_stm32cube_tick_hook;
+
+// vim: path=.,/usr/share/stm32lib/inc/,/usr/arm-none-eabi/include,../../../inc
+
