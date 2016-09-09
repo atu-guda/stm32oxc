f9a981f28cee3193e49d734d694200490e1acefc
Anton Guda <atu@nmetau.edu.ua>
2016-09-09 19:50:16 +0300

    Adopt to to libc, cube.

--- examples/f303dis/18_i2c_ssd1306_oled/main.cpp
+++ examples/f303dis/18_i2c_ssd1306_oled/main.cpp
@@ -1,5 +1,6 @@
 #include <cstring>
 #include <cstdlib>
+#include <cmath>
 #include <algorithm>
 
 #include <oxc_auto.h>
--- examples/f303dis/22_spi_pcd8544_lcd/main.cpp
+++ examples/f303dis/22_spi_pcd8544_lcd/main.cpp
@@ -1,5 +1,7 @@
 #include <cstring>
 #include <cstdlib>
+#include <cmath>
+
 #include <algorithm>
 
 #include <oxc_auto.h>
