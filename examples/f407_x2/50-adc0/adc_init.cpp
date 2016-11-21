46ff00f595a1aff5e22ab787dfb358c7be45d6f1
Anton Guda <atu@nmetau.edu.ua>
2016-11-21 20:47:16 +0200

    Adopt 51-ADC0 example for f407

---
+++ examples/f407_x2/50-adc0/adc_init.cpp
@@ -0,0 +1,86 @@
+#include <oxc_base.h>
+
+extern ADC_HandleTypeDef hadc1;
+//ADC_HandleTypeDef hadc2;
+//ADC_HandleTypeDef hadc3;
+//DMA_HandleTypeDef hdma_adc1;
+
+void MX_ADC1_Init(void)
+{
+  __HAL_RCC_ADC1_CLK_ENABLE();
+  ADC_ChannelConfTypeDef sConfig;
+
+  /**Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)     */
+  hadc1.Instance                   = ADC1;
+  hadc1.Init.ClockPrescaler        = ADC_CLOCK_SYNC_PCLK_DIV4;
+  hadc1.Init.Resolution            = ADC_RESOLUTION_12B;
+  hadc1.Init.ScanConvMode          = DISABLE;
+  hadc1.Init.ContinuousConvMode    = DISABLE;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConvEdge  = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc1.Init.DataAlign             = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.NbrOfConversion       = 1;
+  hadc1.Init.DMAContinuousRequests = DISABLE;
+  hadc1.Init.EOCSelection          = ADC_EOC_SINGLE_CONV;
+  if( HAL_ADC_Init( &hadc1 ) != HAL_OK )  {
+    Error_Handler( 2 );
+  }
+
+  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.  */
+  sConfig.Channel = ADC_CHANNEL_1;
+  sConfig.Rank = 1;
+  // sConfig.SamplingTime = ADC_SAMPLETIME_15CYCLES;
+  sConfig.SamplingTime = ADC_SAMPLETIME_144CYCLES;
+  if( HAL_ADC_ConfigChannel( &hadc1 , &sConfig ) != HAL_OK )  {
+    Error_Handler( 3 );
+  }
+
+}
+
+void HAL_ADC_MspInit(ADC_HandleTypeDef* adcHandle)
+{
+  GPIO_InitTypeDef GPIO_InitStruct;
+  if( adcHandle->Instance==ADC1 )  {
+    __HAL_RCC_ADC1_CLK_ENABLE();
+    __GPIOA_CLK_ENABLE();
+
+    //* ADC1 GPIO Configuration        PA1   ------> ADC1_IN1
+    GPIO_InitStruct.Pin  = GPIO_PIN_1;
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init( GPIOA, &GPIO_InitStruct );
+
+    // #<{(| Peripheral DMA init|)}>#
+    // hdma_adc1.Instance = DMA2_Stream0;
+    // hdma_adc1.Init.Channel = DMA_CHANNEL_0;
+    // hdma_adc1.Init.Direction = DMA_PERIPH_TO_MEMORY;
+    // hdma_adc1.Init.PeriphInc = DMA_PINC_DISABLE;
+    // hdma_adc1.Init.MemInc = DMA_MINC_ENABLE;
+    // hdma_adc1.Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD;
+    // hdma_adc1.Init.MemDataAlignment = DMA_MDATAALIGN_HALFWORD;
+    // hdma_adc1.Init.Mode = DMA_NORMAL;
+    // hdma_adc1.Init.Priority = DMA_PRIORITY_LOW;
+    // hdma_adc1.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
+    // if( HAL_DMA_Init( &hdma_adc1 ) != HAL_OK )   {
+    //   Error_Handler( 6 );
+    // }
+    //
+    // __HAL_LINKDMA(adcHandle,DMA_Handle,hdma_adc1);
+    //
+    // #<{(| Peripheral interrupt init |)}>#
+    // HAL_NVIC_SetPriority(ADC_IRQn, 0, 0);
+    // HAL_NVIC_EnableIRQ(ADC_IRQn);
+
+  }
+}
+
+void HAL_ADC_MspDeInit( ADC_HandleTypeDef* adcHandle )
+{
+  if( adcHandle->Instance == ADC1 )  {
+    __HAL_RCC_ADC1_CLK_DISABLE();
+    HAL_GPIO_DeInit( GPIOA, GPIO_PIN_1 );
+    // HAL_DMA_DeInit(adcHandle->DMA_Handle);
+  }
+
+}
+
---
+++ examples/f407_x2/50-adc0/main.cpp
@@ -0,0 +1,134 @@
+#include <cstring>
+#include <cstdlib>
+
+#include <oxc_auto.h>
+
+using namespace std;
+using namespace SMLRL;
+
+USE_DIE4LED_ERROR_HANDLER;
+
+// PinsOut p1 { GPIOC, 0, 4 };
+BOARD_DEFINE_LEDS;
+
+UsbcdcIO usbcdc;
+
+void MX_ADC1_Init(void);
+ADC_HandleTypeDef hadc1;
+int v_adc_ref = 3250; // in mV, measured before test
+
+
+const int def_stksz = 2 * configMINIMAL_STACK_SIZE;
+
+SmallRL srl( smallrl_exec );
+
+// --- local commands;
+int cmd_test0( int argc, const char * const * argv );
+CmdInfo CMDINFO_TEST0 { "test0", 'T', cmd_test0, " - test something 0"  };
+
+const CmdInfo* global_cmds[] = {
+  DEBUG_CMDS,
+
+  &CMDINFO_TEST0,
+  nullptr
+};
+
+
+extern "C" {
+void task_main( void *prm UNUSED_ARG );
+}
+
+STD_USBCDC_SEND_TASK( usbcdc );
+
+int main(void)
+{
+  HAL_Init();
+
+  SystemClock_Config();
+
+  leds.initHW();
+
+  leds.write( 0x0F );  delay_bad_ms( 200 );
+
+  UVAR('t') = 1000;
+  UVAR('n') = 10;
+  UVAR('v') = v_adc_ref;
+
+  global_smallrl = &srl;
+
+  //           code               name    stack_sz      param  prty TaskHandle_t*
+  xTaskCreate( task_leds,        "leds", 1*def_stksz, nullptr,   1, nullptr );
+  xTaskCreate( task_usbcdc_send, "send", 2*def_stksz, nullptr,   2, nullptr );  // 2
+  xTaskCreate( task_main,        "main", 2*def_stksz, nullptr,   1, nullptr );
+  xTaskCreate( task_gchar,      "gchar", 2*def_stksz, nullptr,   1, nullptr );
+
+  leds.write( 0x00 );
+  ready_to_start_scheduler = 1;
+  vTaskStartScheduler();
+
+  die4led( 0xFF );
+  return 0;
+}
+
+void task_main( void *prm UNUSED_ARG ) // TMAIN
+{
+  SET_USBCDC_AS_STDIO(usbcdc);
+
+  default_main_loop();
+  vTaskDelete(NULL);
+}
+
+// TEST0
+int cmd_test0( int argc, const char * const * argv )
+{
+  char buf[32];
+  MX_ADC1_Init();
+  int n = arg2long_d( 1, argc, argv, UVAR('n'), 0 );
+  uint32_t t_step = UVAR('t');
+  pr( NL "Test0: n= " ); pr_d( n ); pr( " t= " ); pr_d( t_step );
+  pr( NL );
+  uint16_t v = 0;
+
+  // log_add( "Test0 " );
+  TickType_t tc0 = xTaskGetTickCount(), tc00 = tc0;
+
+  break_flag = 0;
+  for( int i=0; i<n && !break_flag; ++i ) {
+    TickType_t tcc = xTaskGetTickCount();
+    pr( "ADC start  i= " ); pr_d( i );
+    pr( "  tick: "); pr_d( tcc - tc00 );
+    if( HAL_ADC_Start( &hadc1 ) != HAL_OK )  {
+      pr( "  !! ADC Start error" NL );
+      break;
+    }
+    HAL_ADC_PollForConversion( &hadc1, 10 );
+    pr( " ADC1.SR= " ); pr_h( ADC1->SR );
+    v = 0;
+    if( HAL_IS_BIT_SET( HAL_ADC_GetState( &hadc1 ), HAL_ADC_STATE_REG_EOC ) )  {
+      v = HAL_ADC_GetValue( &hadc1 );
+      int vv = v * 10 * UVAR('v') / 4096;
+      ifcvt( vv, 10000, buf, 4 );
+      pr( " v= " ); pr_d( v ); pr( " vv= " ); pr( buf );
+    }
+
+    pr( NL );
+    vTaskDelayUntil( &tc0, t_step );
+    // delay_ms( t_step );
+  }
+
+  pr( NL );
+  __HAL_RCC_ADC1_CLK_DISABLE();
+
+  delay_ms( 10 );
+  break_flag = 0;  idle_flag = 1;
+
+  pr( NL "test0 end." NL );
+  return 0;
+}
+
+//  ----------------------------- configs ----------------
+
+FreeRTOS_to_stm32cube_tick_hook;
+
+// vim: path=.,/usr/share/stm32lib/inc/,/usr/arm-none-eabi/include,../../../inc
+
