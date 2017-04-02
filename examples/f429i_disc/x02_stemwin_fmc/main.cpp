93dc336aad13297604b5904209db46a7dedb15d7
Anton Guda <atu@nmetau.edu.ua>
2017-04-02 14:09:04 +0300

    Add initial x02_stemwin_fmc f429_disc

---
+++ examples/f429i_disc/x02_stemwin_fmc/main.cpp
@@ -0,0 +1,203 @@
+#include <cstring>
+#include <cstdlib>
+
+#include <oxc_auto.h>
+
+#include <bsp/board_stm32f429discovery_sdram.h>
+#include <stm32f429i_discovery_ts.h>
+
+#include <GUI.h>
+#include <WM.h>
+
+
+using namespace std;
+using namespace SMLRL;
+
+USE_DIE4LED_ERROR_HANDLER;
+
+BOARD_DEFINE_LEDS;
+
+SDRAM_HandleTypeDef hsdram;
+
+const int def_stksz = 2 * configMINIMAL_STACK_SIZE;
+
+SmallRL srl( smallrl_exec );
+
+// --- local commands;
+int cmd_test0( int argc, const char * const * argv );
+CmdInfo CMDINFO_TEST0 { "test0", 'T', cmd_test0, " - test something 0"  };
+
+const CmdInfo* global_cmds[] = {
+  DEBUG_CMDS,
+
+  &CMDINFO_TEST0,
+  nullptr
+};
+
+uint8_t GUI_Initialized = 0;
+void BSP_Pointer_Update();
+
+extern "C" {
+
+void task_main( void *prm UNUSED_ARG );
+void task_grout( void *prm UNUSED_ARG );
+
+
+}
+
+
+UART_HandleTypeDef uah;
+UsartIO usartio( &uah, USART1 );
+void init_uart( UART_HandleTypeDef *uahp, int baud = 115200 );
+
+STD_USART1_SEND_TASK( usartio );
+// STD_USART1_RECV_TASK( usartio );
+STD_USART1_IRQ( usartio );
+
+int main(void)
+{
+  HAL_Init();
+
+  SystemClock_Config();
+
+  leds.initHW();
+
+  leds.write( 0x03 );  delay_bad_ms( 200 );
+  // leds.write( 0x00 );  HAL_Delay( 200 );
+  leds.write( 0x00 );  delay_ms( 200 );
+  bsp_init_sdram( &hsdram );
+  leds.write( 0x03 );  delay_bad_ms( 200 );
+  init_uart( &uah );
+
+  BSP_TS_Init( 240, 320 );
+  __HAL_RCC_CRC_CLK_ENABLE();
+
+  GUI_Init();
+  GUI_Initialized = 1;
+
+  /* Activate the use of memory device feature */
+  WM_SetCreateFlags( WM_CF_MEMDEV );
+  GUI_Clear();
+  GUI_SetFont( &GUI_Font20_1 );
+  GUI_DispStringAt( "XXX!", (LCD_GetXSize()-100)/2, (LCD_GetYSize()-20)/2 );
+
+  leds.write( 0x01 );  delay_bad_ms( 1000 );
+  // MainTask();
+
+  // HAL_UART_Transmit( &uah, (uint8_t*)"START\r\n", 7, 100 );
+
+  // usartio.sendStrSync( "0123456789---main()---ABCDEF" NL );
+
+  UVAR('t') = 1000;
+  UVAR('n') = 10;
+
+  global_smallrl = &srl;
+
+  //           code               name    stack_sz      param  prty TaskHandle_t*
+  xTaskCreate( task_leds,        "leds", 1*def_stksz, nullptr,   1, nullptr );
+  xTaskCreate( task_usart1_send, "send", 2*def_stksz, nullptr,   2, nullptr );  // 2
+  xTaskCreate( task_main,        "main", 2*def_stksz, nullptr,   1, nullptr );
+  xTaskCreate( task_grout,      "grout", 2*def_stksz, nullptr,   1, nullptr );
+  xTaskCreate( task_gchar,      "gchar", 2*def_stksz, nullptr,   1, nullptr );
+
+  leds.write( 0x00 );
+  ready_to_start_scheduler = 1;
+  vTaskStartScheduler();
+
+  die4led( 0xFF );
+  return 0;
+}
+
+
+void task_main( void *prm UNUSED_ARG ) // TMAIN
+{
+  SET_UART_AS_STDIO( usartio );
+
+  usartio.sendStrSync( "0123456789ABCDEF" NL );
+  delay_ms( 10 );
+
+  default_main_loop();
+  vTaskDelete(NULL);
+}
+
+void task_grout( void *prm UNUSED_ARG )
+{
+  delay_ms( 100 );
+  if( GUI_Initialized )  {
+    GUI_DispStringAt( "Hello world!", (LCD_GetXSize()-100)/2, (LCD_GetYSize()-20)/2 );
+  }
+
+  int i=0;
+  int xsz = LCD_GetXSize(), ysz = LCD_GetYSize(), xysz = xsz * ysz;
+  while( 1 ) {
+    if( GUI_Initialized )  {
+      GUI_DispStringAt( "Z", i % xsz, i / xsz );
+      // BSP_Pointer_Update();
+    }
+    delay_ms( 10 );
+    ++i;
+    i %= xysz;
+    leds.toggle( 0x02 );
+  }
+
+  vTaskDelete(NULL);
+}
+
+void BSP_Pointer_Update()
+{
+  GUI_PID_STATE TS_State;
+  static TS_StateTypeDef prev_state;
+  TS_StateTypeDef  ts;
+  uint16_t xDiff, yDiff;
+
+  BSP_TS_GetState( &ts );
+
+  TS_State.Pressed = ts.TouchDetected;
+
+  xDiff = (prev_state.X > ts.X) ? (prev_state.X - ts.X) : (ts.X - prev_state.X);
+  yDiff = (prev_state.Y > ts.Y) ? (prev_state.Y - ts.Y) : (ts.Y - prev_state.Y);
+
+  if( ts.TouchDetected ) {
+    if( (prev_state.TouchDetected != ts.TouchDetected ) ||  (xDiff > 3 ) || (yDiff > 3))  {
+      prev_state = ts;
+      TS_State.Layer = 0;
+      TS_State.x = ts.X;
+      TS_State.y = ts.Y;
+      GUI_TOUCH_StoreStateEx( &TS_State );
+    }
+  }
+}
+
+
+// TEST0
+int cmd_test0( int argc, const char * const * argv )
+{
+  int n = arg2long_d( 1, argc, argv, UVAR('n'), 0 );
+  pr( NL "Test0: n= " ); pr_d( n );
+  pr( NL );
+  delay_ms( 100 );
+  for( int i=0; i<n; ++i ) {
+    SDRAM_ADDR[i] = (uint8_t)(i+0x20);
+  }
+
+  dump8( SDRAM_ADDR, n, true );
+
+  pr( NL );
+
+  delay_ms( 10 );
+  break_flag = 0;  idle_flag = 1;
+
+  pr( NL "test0 end." NL );
+  return 0;
+}
+
+
+
+//  ----------------------------- configs ----------------
+
+
+
+FreeRTOS_to_stm32cube_tick_hook;
+
+// vim: path=.,/usr/share/stm32lib/inc/,/usr/arm-none-eabi/include,../../../inc
+
