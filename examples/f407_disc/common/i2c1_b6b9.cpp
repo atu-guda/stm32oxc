7942176799c5cbce2017e5a2ce709346a100b355
Anton Guda <atu@nmetau.edu.ua>
2017-05-06 16:26:26 +0300

    Add I2C example for f746_wave.
    I2C init is not common for now.

--- examples/f407_disc/common/i2c1_b6b9.cpp
+++ examples/f407_disc/common/i2c1_b6b9.cpp
@@ -1,58 +1,51 @@
-/**
-  * File Name          : i2c1_b6b9.cpp
-  * Description        : Default initialization for I2C1 on f4discovery
-  */
-
 #include <oxc_base.h>
 #include <oxc_gpio.h>
 
 void MX_I2C1_Init( I2C_HandleTypeDef &i2c, uint32_t speed  )
 {
-  i2c.Instance             = I2C1;
-  i2c.State                = HAL_I2C_STATE_RESET;
-  i2c.Init.AddressingMode  = I2C_ADDRESSINGMODE_7BIT;
-  i2c.Init.ClockSpeed      = speed;
-  i2c.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-  i2c.Init.DutyCycle       = I2C_DUTYCYCLE_16_9;
-  i2c.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-  i2c.Init.NoStretchMode   = I2C_NOSTRETCH_DISABLE;
-  i2c.Init.OwnAddress1     = 0;
-  i2c.Init.OwnAddress2     = 0;
+  i2c.Instance              = I2C1;
+  i2c.Init.ClockSpeed       = speed;
+  i2c.State                 = HAL_I2C_STATE_RESET;
+  i2c.Init.AddressingMode   = I2C_ADDRESSINGMODE_7BIT;
+  i2c.Init.DualAddressMode  = I2C_DUALADDRESS_DISABLE;
+  i2c.Init.DutyCycle        = I2C_DUTYCYCLE_16_9;
+  i2c.Init.GeneralCallMode  = I2C_GENERALCALL_DISABLE;
+  i2c.Init.NoStretchMode    = I2C_NOSTRETCH_DISABLE;
+  i2c.Init.OwnAddress1      = 0;
+  i2c.Init.OwnAddress2      = 0;
   HAL_I2C_Init( &i2c );
 }
 
-void HAL_I2C_MspInit( I2C_HandleTypeDef* hi2c )
+
+void HAL_I2C_MspInit( I2C_HandleTypeDef *hi2c )
 {
-  GPIO_InitTypeDef gpi;
-  // leds.toggle( BIT2 );
+  GPIO_InitTypeDef gio;
   if( hi2c->Instance == I2C1 ) {
-    // leds.toggle( BIT0 );
     __I2C1_CLK_ENABLE();
     __GPIOB_CLK_ENABLE();
 
-    // PB6 --> I2C1_SCL
-    // PB9 --> I2C1_SDA
-    gpi.Pin = GPIO_PIN_6 | GPIO_PIN_9;
-    gpi.Mode = GPIO_MODE_AF_OD;
-    gpi.Pull = GPIO_NOPULL;
-    gpi.Speed = GPIO_SPEED_MAX;
-    gpi.Alternate = GPIO_AF4_I2C1;
-    HAL_GPIO_Init( GPIOB, &gpi );
+    // B6 --> I2C1_SCL
+    // B9 --> I2C1_SDA
+    gio.Pin       = GPIO_PIN_6 | GPIO_PIN_9;
+    gio.Mode      = GPIO_MODE_AF_OD;
+    gio.Pull      = GPIO_NOPULL;
+    gio.Speed     = GPIO_SPEED_MAX;
+    gio.Alternate = GPIO_AF4_I2C1;
+    HAL_GPIO_Init( GPIOB, &gio );
 
     // tmp: not now
     // HAL_NVIC_SetPriority( I2C1_EV_IRQn, 15, 0 ); // TODO: from FreeRTOS
     // HAL_NVIC_EnableIRQ( I2C1_EV_IRQn );
   }
-
 }
 
-void HAL_I2C_MspDeInit(I2C_HandleTypeDef* hi2c)
+void HAL_I2C_MspDeInit( I2C_HandleTypeDef *hi2c )
 {
-  if( hi2c->Instance==I2C1 )  {
+  if( hi2c->Instance == I2C1 ) {
     __I2C1_CLK_DISABLE();
     HAL_GPIO_DeInit( GPIOB, GPIO_PIN_6 | GPIO_PIN_9 );
     HAL_NVIC_DisableIRQ( I2C1_EV_IRQn );
   }
-
 }
+
 
--- examples/f746_wave/common/i2c1_init_b8b9.cpp
+++ examples/f746_wave/common/i2c1_init_b8b9.cpp
@@ -4,32 +4,33 @@
 void MX_I2C1_Init( I2C_HandleTypeDef &i2c )
 {
   i2c.Instance              = I2C1;
-  i2c.Init.Timing           = 0x10808DD3;
-  // i2c.State                 = HAL_I2C_STATE_RESET;
+  i2c.Init.Timing           = 0x30C0EDFF; // 100 kHz over 200 MHz
+  i2c.State                 = HAL_I2C_STATE_RESET;
   i2c.Init.AddressingMode   = I2C_ADDRESSINGMODE_7BIT;
-  i2c.Init.DualAddressMode  = I2C_DUALADDRESS_DISABLED;
+  i2c.Init.DualAddressMode  = I2C_DUALADDRESS_DISABLE;
   // i2c.Init.DutyCycle        = I2C_DUTYCYCLE_16_9;
-  i2c.Init.GeneralCallMode  = I2C_GENERALCALL_DISABLED;
-  i2c.Init.NoStretchMode    = I2C_NOSTRETCH_DISABLED;
+  i2c.Init.GeneralCallMode  = I2C_GENERALCALL_DISABLE;
+  i2c.Init.NoStretchMode    = I2C_NOSTRETCH_DISABLE;
   i2c.Init.OwnAddress1      = 0;
   i2c.Init.OwnAddress2      = 0;
   i2c.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
   HAL_I2C_Init( &i2c );
 
   HAL_I2CEx_AnalogFilter_Config( &i2c, I2C_ANALOGFILTER_ENABLED );
+  HAL_I2CEx_ConfigDigitalFilter( &i2c, 0 );
 }
 
 
-void HAL_I2C_MspInit( I2C_HandleTypeDef *hi2c )
+void HAL_I2C_MspInit( I2C_HandleTypeDef *i2c )
 {
   GPIO_InitTypeDef gio;
-  if( hi2c->Instance == I2C1 ) {
+  if( i2c->Instance == I2C1 ) {
     __I2C1_CLK_ENABLE();
     __GPIOB_CLK_ENABLE();
 
-    // B6 --> I2C1_SCL
-    // B7 --> I2C1_SDA
-    gio.Pin       = GPIO_PIN_6 | GPIO_PIN_7;
+    // B8 --> I2C1_SCL
+    // B9 --> I2C1_SDA
+    gio.Pin       = GPIO_PIN_8 | GPIO_PIN_9;
     gio.Mode      = GPIO_MODE_AF_OD;
     gio.Pull      = GPIO_NOPULL;
     gio.Speed     = GPIO_SPEED_MAX;
@@ -38,11 +39,11 @@ void HAL_I2C_MspInit( I2C_HandleTypeDef *hi2c )
   }
 }
 
-void HAL_I2C_MspDeInit( I2C_HandleTypeDef *hi2c )
+void HAL_I2C_MspDeInit( I2C_HandleTypeDef *i2c )
 {
-  if( hi2c->Instance == I2C1 ) {
+  if( i2c->Instance == I2C1 ) {
     __I2C1_CLK_DISABLE();
-    HAL_GPIO_DeInit( GPIOB, GPIO_PIN_6 | GPIO_PIN_7 );
+    HAL_GPIO_DeInit( GPIOB, GPIO_PIN_8 | GPIO_PIN_8 );
     HAL_NVIC_DisableIRQ( I2C1_EV_IRQn );
   }
 }
