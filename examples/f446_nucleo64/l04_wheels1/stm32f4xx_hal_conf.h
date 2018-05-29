31bfbbfb86e352828bfc7a2f45d33690186698d0
Anton Guda <atu@nmetau.edu.ua>
2018-05-30 00:14:43 +0300

    wheels example: first steps

--- examples/f446_nucleo64/l04_wheels1/stm32f4xx_hal_conf.h
+++ examples/f446_nucleo64/l04_wheels1/stm32f4xx_hal_conf.h
@@ -24,6 +24,7 @@
 #define HAL_DMA_MODULE_ENABLED
 #define HAL_UART_MODULE_ENABLED
 #define HAL_I2C_MODULE_ENABLED
+#define HAL_TIM_MODULE_ENABLED
 
 #include "../common/stm32f4xx_hal_conf_common.h"
 
