d868ab20360bff8c875c5eaa6c94024fdabf9fdc
Anton Guda <atu@nmetau.edu.ua>
2018-06-01 19:05:50 +0300

    Add PWM for motors

--- examples/f446_nucleo64/l04_wheels1/main.cpp
+++ examples/f446_nucleo64/l04_wheels1/main.cpp
@@ -3,6 +3,8 @@
 
 #include <oxc_auto.h>
 
+#include "wheels_pins.h"
+
 using namespace std;
 using namespace SMLRL;
 
@@ -18,6 +20,13 @@ PinsOut motor_dir( GPIOC, 5, 5 );
 const int motor_bits_r = 0x03; // bit 0x04 is reserved
 const int motor_bits_l = 0x18;
 const int motor_bits   = motor_bits_r | motor_bits_l;
+
+void HAL_TIM_MspPostInit( TIM_HandleTypeDef* timHandle );
+uint32_t get_TIM1_8_in_freq();
+void tim1_cfg();
+TIM_HandleTypeDef tim1_h;
+const int tim1_period = 8500; // approx 20Hz
+void set_motor_pwm( int r, int l );
 
 // --- local commands;
 int cmd_test0( int argc, const char * const * argv );
@@ -46,10 +55,11 @@ int main(void)
   UVAR('t') = 1000;
   UVAR('n') = 10;
 
+  motor_dir.initHW();
   motor_dir.reset( 0x1F );
 
-  UVAR('e') = i2c_default_init( i2ch /*, 400000 */ );
-  i2c_dbg = &i2cd;
+  // UVAR('e') = i2c_default_init( i2ch /*, 400000 */ );
+  // i2c_dbg = &i2cd;
 
   BOARD_POST_INIT_BLINK;
 
@@ -61,6 +71,7 @@ int main(void)
 
 void task_main( void *prm UNUSED_ARG ) // TMAIN
 {
+  tim1_cfg();
   default_main_loop();
   vTaskDelete(NULL);
 }
@@ -76,17 +87,38 @@ int cmd_test0( int argc, const char * const * argv )
   return 0;
 }
 
+void set_motor_pwm( int r, int l )
+{
+  HAL_TIM_PWM_Stop( &tim1_h, TIM_CHANNEL_1 );
+  HAL_TIM_PWM_Stop( &tim1_h, TIM_CHANNEL_2 );
+  TIM_OC_InitTypeDef tim_oc_cfg;
+  tim_oc_cfg.OCMode       = TIM_OCMODE_PWM1;
+  tim_oc_cfg.OCPolarity   = TIM_OCPOLARITY_HIGH;
+  tim_oc_cfg.OCNPolarity  = TIM_OCNPOLARITY_LOW;
+  tim_oc_cfg.OCFastMode   = TIM_OCFAST_DISABLE;
+  tim_oc_cfg.OCIdleState  = TIM_OCIDLESTATE_RESET;
+  tim_oc_cfg.OCNIdleState = TIM_OCNIDLESTATE_RESET;
+  tim_oc_cfg.Pulse = tim1_period * abs(r) / 100;
+  HAL_TIM_PWM_ConfigChannel( &tim1_h, &tim_oc_cfg, TIM_CHANNEL_1 );
+  tim_oc_cfg.Pulse = tim1_period * abs(l) / 100;
+  HAL_TIM_PWM_ConfigChannel( &tim1_h, &tim_oc_cfg, TIM_CHANNEL_2 );
+  HAL_TIM_PWM_Start( &tim1_h, TIM_CHANNEL_1 );
+  HAL_TIM_PWM_Start( &tim1_h, TIM_CHANNEL_2 );
+}
+
 int cmd_go( int argc, const char * const * argv )
 {
   int t    = arg2long_d( 1, argc, argv, 1000,  0, 10000 );
   int r_w  = arg2long_d( 2, argc, argv,   50, -100, 100 );
-  int l_w  = arg2long_d( 2, argc, argv,  r_w, -100, 100 );
+  int l_w  = arg2long_d( 3, argc, argv,  r_w, -100, 100 );
   pr( NL "go: t= " ); pr_d( t ); pr( " r= " ); pr_d( r_w ); pr( " l= " ); pr_d( l_w ); pr ( NL );
 
   uint8_t bits = r_w > 0 ?    1 : 0;
   bits        |= r_w < 0 ?    2 : 0;
   bits        |= l_w > 0 ?    8 : 0;
   bits        |= l_w < 0 ? 0x10 : 0;
+
+  set_motor_pwm( r_w, l_w );
 
   motor_dir.write( bits );
 
@@ -95,6 +127,7 @@ int cmd_go( int argc, const char * const * argv )
   }
 
   motor_dir.reset( motor_bits );
+  set_motor_pwm( 0, 0 );
   if( break_flag ) {
     pr( "Break!" NL );
   }
@@ -102,6 +135,133 @@ int cmd_go( int argc, const char * const * argv )
   return 0;
 }
 
+uint32_t get_TIM1_8_in_freq()
+{
+  uint32_t hclk  = HAL_RCC_GetHCLKFreq();
+  uint32_t pclk2 = HAL_RCC_GetPCLK2Freq(); // for TIM1, 8
+  if( hclk != pclk2 ) { // *2 : if APB2 prescaler != 1 (=2)
+    pclk2 *= 2;
+  }
+  return pclk2;
+}
+
+void tim1_cfg()
+{
+  tim1_h.Instance               = TIM1;
+  // US defines tick: 5.8 mks approx 1mm 170000 = v_c/2 in mm/s, 998 or 846
+  tim1_h.Init.Prescaler         = get_TIM1_8_in_freq() / 170000 - 1;
+  tim1_h.Init.Period            = tim1_period; // F approx 20Hz: for  motor PWM
+  tim1_h.Init.ClockDivision     = 0;
+  tim1_h.Init.CounterMode       = TIM_COUNTERMODE_UP;
+  tim1_h.Init.RepetitionCounter = 0;
+  if( HAL_TIM_Base_Init( &tim1_h ) != HAL_OK ) {
+    UVAR('e') = 111; // like error
+    return;
+  }
+
+  TIM_ClockConfigTypeDef sClockSourceConfig;
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  HAL_TIM_ConfigClockSource( &tim1_h, &sClockSourceConfig );
+
+  HAL_TIM_PWM_Init( &tim1_h );
+
+  set_motor_pwm( 0, 0 );
+
+  HAL_TIM_PWM_Stop( &tim1_h, TIM_CHANNEL_3 );
+
+  TIM_OC_InitTypeDef tim_oc_cfg;
+  tim_oc_cfg.OCMode       = TIM_OCMODE_PWM1;
+  tim_oc_cfg.OCPolarity   = TIM_OCPOLARITY_HIGH;
+  tim_oc_cfg.OCNPolarity  = TIM_OCNPOLARITY_LOW;
+  tim_oc_cfg.OCFastMode   = TIM_OCFAST_DISABLE;
+  tim_oc_cfg.OCIdleState  = TIM_OCIDLESTATE_RESET;
+  tim_oc_cfg.OCNIdleState = TIM_OCNIDLESTATE_RESET;
+  tim_oc_cfg.Pulse = 3; // 16 us
+  HAL_TIM_PWM_ConfigChannel( &tim1_h, &tim_oc_cfg, TIM_CHANNEL_3 );
+
+  HAL_TIM_PWM_Start( &tim1_h, TIM_CHANNEL_3 );
+}
+
+void TIM1_CC_IRQHandler(void)
+{
+  HAL_TIM_IRQHandler( &tim1_h );
+  leds.toggle( BIT2 );
+}
+
+void HAL_TIM_Base_MspInit( TIM_HandleTypeDef* tim_baseHandle )
+{
+  GPIO_InitTypeDef gio;
+  if( tim_baseHandle->Instance == TIM1 ) {
+    __HAL_RCC_TIM1_CLK_ENABLE();
+    /** TIM1:  PA11     ------> TIM1_CH4     */
+    gio.Pin       = T1_4_US_Echo_Pin;
+    gio.Mode      = GPIO_MODE_AF_PP;
+    gio.Pull      = GPIO_NOPULL;
+    gio.Speed     = GPIO_SPEED_FREQ_LOW;
+    gio.Alternate = GPIO_AF1_TIM1;
+    HAL_GPIO_Init( T1_4_US_Echo_GPIO_Port, &gio );
+
+    HAL_NVIC_SetPriority(TIM1_CC_IRQn, 5, 0); // TODO: coorect for FreeRTOS
+    HAL_NVIC_EnableIRQ( TIM1_CC_IRQn );
+  }
+  else if( tim_baseHandle->Instance == TIM3 ) {
+    __HAL_RCC_TIM3_CLK_ENABLE();
+    /**TIM3:  PA6     ------> TIM3_CH1    PA7     ------> TIM3_CH2    */
+    gio.Pin       = T3_1_M_count_R_Pin | T3_2_count_l_Pin;
+    gio.Mode      = GPIO_MODE_AF_PP;
+    gio.Pull      = GPIO_NOPULL;
+    gio.Speed     = GPIO_SPEED_FREQ_LOW;
+    gio.Alternate = GPIO_AF2_TIM3;
+    HAL_GPIO_Init( GPIOA, &gio );
+
+    HAL_NVIC_SetPriority( TIM3_IRQn, 5, 0 );
+    HAL_NVIC_EnableIRQ(TIM3_IRQn);
+  }
+  else if( tim_baseHandle->Instance == TIM4 ) {
+    __HAL_RCC_TIM4_CLK_ENABLE();
+  }
+  HAL_TIM_MspPostInit( tim_baseHandle );
+}
+
+void HAL_TIM_MspPostInit( TIM_HandleTypeDef* timHandle )
+{
+  GPIO_InitTypeDef gio;
+  if( timHandle->Instance == TIM1 )  {
+    /**TIM1 GPIO Configuration
+     PA8     ------> TIM1_CH1
+     PA9     ------> TIM1_CH2
+     PA10     ------> TIM1_CH3     */
+    gio.Pin = T1_1_M_Right_Pin | T1_2_M_Left_Pin | T1_3_US_Pulse_Pin;
+    gio.Mode      = GPIO_MODE_AF_PP;
+    gio.Pull      = GPIO_NOPULL;
+    gio.Speed     = GPIO_SPEED_FREQ_VERY_HIGH;
+    gio.Alternate = GPIO_AF1_TIM1;
+    HAL_GPIO_Init( GPIOA, &gio );
+  } else if( timHandle->Instance == TIM4 )  {
+    /** TIM4 GPIO Configuration   PB6     ------> TIM4_CH1     */
+    gio.Pin       = T4_1_servo_Pin;
+    gio.Mode      = GPIO_MODE_AF_PP;
+    gio.Pull      = GPIO_NOPULL;
+    gio.Speed     = GPIO_SPEED_FREQ_LOW;
+    gio.Alternate = GPIO_AF2_TIM4;
+    HAL_GPIO_Init( T4_1_servo_GPIO_Port, &gio );
+  }
+}
+
+void HAL_TIM_Base_MspDeInit( TIM_HandleTypeDef* tim_baseHandle )
+{
+  if( tim_baseHandle->Instance == TIM1 )   {
+    HAL_NVIC_DisableIRQ( TIM1_CC_IRQn );
+    __HAL_RCC_TIM1_CLK_DISABLE();
+    HAL_GPIO_DeInit( GPIOA, T1_1_M_Right_Pin | T1_2_M_Left_Pin | T1_3_US_Pulse_Pin | T1_4_US_Echo_Pin );
+  } else if(tim_baseHandle->Instance == TIM3 ) {
+    HAL_NVIC_DisableIRQ(TIM3_IRQn);
+    __HAL_RCC_TIM3_CLK_DISABLE();
+    HAL_GPIO_DeInit(GPIOA, T3_1_M_count_R_Pin|T3_2_count_l_Pin);
+  } else if( tim_baseHandle->Instance == TIM4 ) {
+    __HAL_RCC_TIM4_CLK_DISABLE();
+  }
+}
 
 // vim: path=.,/usr/share/stm32cube/inc/,/usr/arm-none-eabi/include,/usr/share/stm32oxc/inc
 
---
+++ examples/f446_nucleo64/l04_wheels1/wheels_pins.h
@@ -0,0 +1,58 @@
+#ifndef _WHEELS_PINS_H
+#define _WHEELS_PINS_H
+
+#define B1_Pin GPIO_PIN_13
+#define B1_GPIO_Port GPIOC
+#define LED0_Pin GPIO_PIN_0
+#define LED0_GPIO_Port GPIOC
+#define LED1_Pin GPIO_PIN_1
+#define LED1_GPIO_Port GPIOC
+#define LED2_Pin GPIO_PIN_2
+#define LED2_GPIO_Port GPIOC
+#define LED3_Pin GPIO_PIN_3
+#define LED3_GPIO_Port GPIOC
+#define USART_TX_Pin GPIO_PIN_2
+#define USART_TX_GPIO_Port GPIOA
+#define USART_RX_Pin GPIO_PIN_3
+#define USART_RX_GPIO_Port GPIOA
+#define T3_1_M_count_R_Pin GPIO_PIN_6
+#define T3_1_M_count_R_GPIO_Port GPIOA
+#define T3_2_count_l_Pin GPIO_PIN_7
+#define T3_2_count_l_GPIO_Port GPIOA
+#define M_dir_r_Pin GPIO_PIN_5
+#define M_dir_r_GPIO_Port GPIOC
+#define prox_fr_Pin GPIO_PIN_12
+#define prox_fr_GPIO_Port GPIOB
+#define prox_frB13_Pin GPIO_PIN_13
+#define prox_frB13_GPIO_Port GPIOB
+#define prox_br_Pin GPIO_PIN_14
+#define prox_br_GPIO_Port GPIOB
+#define prox_bl_Pin GPIO_PIN_15
+#define prox_bl_GPIO_Port GPIOB
+#define M_dir_rn_Pin GPIO_PIN_6
+#define M_dir_rn_GPIO_Port GPIOC
+#define M_dir_aux_Pin GPIO_PIN_7
+#define M_dir_aux_GPIO_Port GPIOC
+#define M_dir_l_Pin GPIO_PIN_8
+#define M_dir_l_GPIO_Port GPIOC
+#define M_dir_ln_Pin GPIO_PIN_9
+#define M_dir_ln_GPIO_Port GPIOC
+#define T1_1_M_Right_Pin GPIO_PIN_8
+#define T1_1_M_Right_GPIO_Port GPIOA
+#define T1_2_M_Left_Pin GPIO_PIN_9
+#define T1_2_M_Left_GPIO_Port GPIOA
+#define T1_3_US_Pulse_Pin GPIO_PIN_10
+#define T1_3_US_Pulse_GPIO_Port GPIOA
+#define T1_4_US_Echo_Pin GPIO_PIN_11
+#define T1_4_US_Echo_GPIO_Port GPIOA
+#define TMS_Pin GPIO_PIN_13
+#define TMS_GPIO_Port GPIOA
+#define TCK_Pin GPIO_PIN_14
+#define TCK_GPIO_Port GPIOA
+#define SWO_Pin GPIO_PIN_3
+#define SWO_GPIO_Port GPIOB
+#define T4_1_servo_Pin GPIO_PIN_6
+#define T4_1_servo_GPIO_Port GPIOB
+
+#endif
+
