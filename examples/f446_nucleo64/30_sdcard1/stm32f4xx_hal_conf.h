9d88128150447584c918cac2c42a29bb99180c09
Anton Guda <atu@nmetau.edu.ua>
2018-07-04 12:20:04 +0300

    Use stm32_hal_conf_base.h for 446_nucleo examples

--- examples/f446_nucleo64/30_sdcard1/stm32f4xx_hal_conf.h
+++ examples/f446_nucleo64/30_sdcard1/stm32f4xx_hal_conf.h
@@ -1,31 +1,16 @@
-/**
-  * @file    stm32f4xx_hal_conf.h
-  * @brief   HAL configuration file. mod by atu.
-  */
-
 #ifndef __STM32F4xx_HAL_CONF_H
 #define __STM32F4xx_HAL_CONF_H
-
-// for oxc lib: just to check
-#define REQ_MCBASE STM32F4
-#define REQ_SYSCLK_FREQ 168
-
-#include <bsp/board_stm32f446_nucleo64.h>
-
-// now even for FreeRTOS
-#define STD_SYSTICK_HANDLER 1
 
 // ########################## Module Selection ##############################
 // base modules
 #define USE_COMMON_HAL_MODULES
+#define USE_COMMON_CONSOLE_MODULES
 
 // AUX modules : full list: ../common/hal_modules_list.h
 //
-#define HAL_DMA_MODULE_ENABLED
-#define HAL_UART_MODULE_ENABLED
 #define HAL_SD_MODULE_ENABLED
 
-#include "../common/stm32f4xx_hal_conf_common.h"
+#include "../common/stm32_hal_conf_base.h"
 
 
 #endif /* __STM32F4xx_HAL_CONF_H */
---
+++ examples/f446_nucleo64/common/stm32_hal_conf_base.h
@@ -0,0 +1,27 @@
+#ifndef __STM32_HAL_CONF_H
+#define __STM32_HAL_CONF_H
+
+// base configuaration file for oxc f446_nucleo examples
+
+#define REQ_MCBASE STM32F4
+#ifndef REQ_SYSCLK_FREQ
+  #define REQ_SYSCLK_FREQ 168
+#endif
+
+#define STD_SYSTICK_HANDLER 1
+
+#define USE_COMMON_HAL_MODULES
+
+#ifdef USE_COMMON_CONSOLE_MODULES
+  #define HAL_UART_MODULE_ENABLED
+  #define HAL_DMA_MODULE_ENABLED
+#endif
+
+#include <bsp/board_stm32f446_nucleo64.h>
+
+#include "stm32f4xx_hal_conf_common.h"
+
+
+#endif /* __STM32_HAL_CONF_H */
+
+
