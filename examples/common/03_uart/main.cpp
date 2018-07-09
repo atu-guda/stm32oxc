f5d0489e306e43cf49a1833fb521860e9af6cd60
Anton Guda <atu@nmetau.edu.ua>
2018-07-09 12:03:23 +0300

    Better 03_uart example

--- examples/common/03_uart/main.cpp
+++ examples/common/03_uart/main.cpp
@@ -33,38 +33,42 @@ int main(void)
 void task_send( void *prm UNUSED_ARG )
 {
   char c = '?', cn = '0';
-  static const char ostr[] = "ABCDE <.> -----\r\n";
+  static const char ostr[] = "A:Z <.> [  ] -----\r\n";
   //                          0123456789ABCDEF1011
   static const auto sz_oszr = strlen( ostr );
+  const unsigned c_pos = 5, hex_pos = 9, read_pos = 13, ovr_pos = 14, i_pos = 16;
 
   int n = 0;
   while( 1 ) {
     bool was_action = false;
     strcpy( tx_buf, ostr );
-    // leds.toggle( BIT2 );
-    tx_buf[10] = cn++;
+
+    tx_buf[i_pos] = cn++;
     if( cn >= 0x7F ) { cn = ' '; }
 
     if( __HAL_USART_GET_FLAG( &uah, UART_FLAG_ORE ) ) { // overrun
       __HAL_USART_CLEAR_OREFLAG( &uah );
       was_action = true;
-      tx_buf[13] = 'O';
+      tx_buf[ovr_pos] = 'O';
       leds.toggle( BIT1 );
     }
 
     if( __HAL_USART_GET_FLAG( &uah, UART_FLAG_RXNE ) ) {
       c = uah.Instance->USART_RX_REG;
       was_action = true;
-      // leds.toggle( BIT2 );
+      leds.toggle( BIT2 );
+      char2hex( c, tx_buf + hex_pos ); tx_buf[hex_pos+2] = ']';
       if( int8_t(c) >= ' ' ) {
-        tx_buf[7]  = c;
+        tx_buf[c_pos]  = c;
       }
-      tx_buf[12] = 'R';
+      tx_buf[read_pos] = 'R';
     }
 
     if( was_action || ( n % 1000000 == 0 ) ) {
       if( HAL_UART_Transmit( &uah, (uint8_t*)tx_buf, sz_oszr, 10 ) != HAL_OK ) {
         leds.toggle( BIT0 );
+      } else {
+        leds.toggle( BIT3 );
       }
     }
     ++n;
