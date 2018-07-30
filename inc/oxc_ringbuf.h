bc5bfa499f83792c6b4ec9284ff43f453e3149fc
Anton Guda <atu@nmetau.edu.ua>
2018-07-30 11:32:35 +0300

    Pull RingBuf changes from test project

--- inc/oxc_ringbuf.h
+++ inc/oxc_ringbuf.h
@@ -1,11 +1,14 @@
 #ifndef _OXC_RINGBUF_H
 #define _OXC_RINGBUF_H
 
+#include <stdint.h>
+
 #ifdef USE_OXC
 #include <oxc_base.h>
+#else
+#include "oxc_extdef.h"
 #endif
 
-// TODO: classes for Mu_t handling: move to oxc_main or oxc_mu
 
 class Chst { // char + status
   public:
@@ -22,7 +25,7 @@ class Chst { // char + status
    char c;
    uint8_t st;
 };
-static_assert( sizeof(Chst) == 2 );
+static_assert( sizeof(Chst) == 2, "Bad size of Chst struct, !=2" );
 
 class RingBuf {
   public:
@@ -34,34 +37,38 @@ class RingBuf {
    unsigned size() const { return sz; } // w/o block, only info
    unsigned capacity() const { return cap; }
    unsigned isFull() const { return sz == cap; }
-   bool put( char c ); // blocks, wait
-   bool tryPut( char c ); // noblocks, fail if busy
-   bool waitPut( char c, uint32_t ms = 100 ); // wait + try, delay_ms(1)
-   unsigned puts( const char *s ); // blocks, in one lock, ret: number of char
-   unsigned puts( const char *s, unsigned l ); // blocks, given length
-   unsigned tryPuts( const char *s ); // try-blocks
-   unsigned tryPuts( const char *s, unsigned l ); // try-blocks, given length
+   int put( char c ); // blocks, wait
+   int tryPut( char c ); // noblocks, fail if busy
+   int puts( const char *s ); // blocks, in one lock, ret: number of char or error
+   int puts( const char *s, unsigned l ); // blocks, given length
+   int puts_ato( const char *s ); // blocks, all or none
+   int puts_ato( const char *s, unsigned l ); // blocks, all or none
+   int tryPuts( const char *s ); // try-blocks, as many as possible
+   int tryPuts( const char *s, unsigned l ); // try-blocks, given length
    Chst get(); // blocks
    Chst tryGet(); // noblocks
-   Chst waitGet( uint32_t ms = 100 ); // wait + try
    unsigned gets( char *d, unsigned max_len );
    unsigned tryGets( char *d, unsigned max_len );
    void reset() { MuLock lock( mu ); reset_nolock(); };
    void reset_nolock() { s = e = sz = 0; }
+   unsigned set_n_wait( unsigned n ) { unsigned tmp = n_wait; n_wait = n; return tmp; }
+   static void set_wait_fun( void (*vf)(void) ) { wait_fun = vf; }
   protected:
-   bool put_nolock( char c );
+   int  put_nolock( char c ); // false = fail due to full
    Chst get_nolock();
-   unsigned puts_nolock( const char *s );
-   unsigned puts_nolock( const char *s, unsigned l );
-   unsigned gets_nolock( char *d, unsigned max_len );
+   int puts_nolock( const char *s );
+   int puts_nolock( const char *s, unsigned l );
+   int gets_nolock( char *d, unsigned max_len );
   protected:
    char *b;
    const unsigned cap;        //* capacity
    volatile unsigned sz = 0;  //* size
    volatile unsigned s  = 0;  //* start index (over the head)
    volatile unsigned e  = 0;  //* end index
+   unsigned n_wait      = 1000; //* number of wait ticks (ms by default)
    Mu_t mu = Mu_init;
    bool was_alloc = false;
+   static void (*wait_fun)(void);
 };
 
 #endif
