bf48bfceba9690811bd8121fa06d6e3ea5ee0866
Anton Guda <atu@nmetau.edu.ua>
2018-08-06 17:23:46 +0300

    Add 05c_def_cons_nort example
    on every live platform

---
+++ examples/common/05c_def_cons_nort/main.cpp
@@ -0,0 +1,89 @@
+#include <cstring>
+
+
+#include <oxc_auto.h>
+
+using namespace std;
+using namespace SMLRL;
+
+
+USE_DIE4LED_ERROR_HANDLER;
+BOARD_DEFINE_LEDS;
+
+BOARD_CONSOLE_DEFINES;
+
+#if defined(USE_OXC_CONSOLE_UART)
+  const char* common_help_string = "Appication to test common console with auto I/O selection: UART" NL;
+#elif defined(USE_OXC_CONSOLE_USB_CDC)
+  const char* common_help_string = "Appication to test common console with auto I/O selection: USB_CDC" NL;
+#else
+  const char* common_help_string = "Appication to test common console with auto I/O selection: Unknown" NL;
+#endif
+
+// --- local commands;
+int cmd_test0( int argc, const char * const * argv );
+CmdInfo CMDINFO_TEST0 { "test0", 'T', cmd_test0, " - test something 0"  };
+
+const CmdInfo* global_cmds[] = {
+  DEBUG_CMDS,
+
+  &CMDINFO_TEST0,
+  nullptr
+};
+
+void idle_main_task()
+{
+  leds.toggle( 1 );
+}
+
+
+
+int main(void)
+{
+  BOARD_PROLOG;
+
+  UVAR('t') = 100;
+  UVAR('n') =  20;
+
+  BOARD_POST_INIT_BLINK;
+
+  pr( NL "##################### " PROJ_NAME NL );
+
+  srl.re_ps();
+
+  oxc_add_aux_tick_fun( led_task_nortos );
+
+  std_main_loop_nortos( &srl, idle_main_task );
+
+  return 0;
+}
+
+// TEST0
+int cmd_test0( int argc, const char * const * argv )
+{
+  STD_os;
+  int n = arg2long_d( 1, argc, argv, UVAR('n'), 0 );
+  uint32_t t_step = UVAR('t');
+  os << NL "Test0: n= " <<  n <<  " t= " << t_step << NL;
+  os.flush();
+
+  uint32_t tm0 = HAL_GetTick(), tm00 = tm0;
+
+  break_flag = 0;
+  for( int i=0; i<n && !break_flag; ++i ) {
+    uint32_t tmc = HAL_GetTick();
+    os << " Fake Action i= "  << i <<  " tick: " << ( tmc - tm00 ) << NL;
+    os.flush();
+    if( UVAR('w') ) {
+       dev_console.wait_eot();
+    }
+    // delay_ms( 3 );
+    delay_ms_until_brk( &tm0, t_step );
+    // delay_ms_brk( t_step );
+  }
+
+  return 0;
+}
+
+// vim: path=.,/usr/share/stm32cube/inc/,/usr/arm-none-eabi/include,/usr/share/stm32oxc/inc
+
