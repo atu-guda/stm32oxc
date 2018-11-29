325e1b005782c449c9e2fb967bb2f8b9b36d165c
Anton Guda <atu@nmetau.edu.ua>
2018-11-29 19:21:43 +0200

    Add 50a_adcdma_soft1 example for f407v_bb0
    Not all ADC examples is alive for now - still need rework.

---
+++ examples/common/50a_adcdma_soft1/main.cpp
@@ -0,0 +1,203 @@
+#include <cstring>
+#include <cstdlib>
+#include <cstdio>
+#include <cerrno>
+
+#include <vector>
+
+#include <oxc_auto.h>
+
+using namespace std;
+using namespace SMLRL;
+
+USE_DIE4LED_ERROR_HANDLER;
+BOARD_DEFINE_LEDS;
+
+BOARD_CONSOLE_DEFINES;
+
+
+OutStream& operator<<( OutStream &os, float rhs );
+
+OutStream& operator<<( OutStream &os, float rhs ) // TODO: to library
+{
+  char buf[32];
+
+  snprintf( buf, sizeof(buf), "%#g", (double)rhs );
+  // snprintf( buf, sizeof(buf), "%16.6e", rhs );
+  os << buf;
+  return os;
+}
+
+
+int adc_init_exa_4ch_manual( ADC_Info &adc, uint32_t adc_presc, uint32_t sampl_cycl, uint8_t n_ch );
+
+ADC_Info adc;
+
+int v_adc_ref = BOARD_ADC_COEFF; // in mV, measured before test, adjust as UVAR('v')
+const uint32_t n_ADC_ch_max = 4; // current - in UVAR('c')
+
+uint16_t ADC_buf[32];
+
+
+const int pbufsz = 128;
+// FIL out_file;
+
+// --- local commands;
+int cmd_test0( int argc, const char * const * argv );
+CmdInfo CMDINFO_TEST0 { "test0", 'T', cmd_test0, " - test ADC"  };
+
+const CmdInfo* global_cmds[] = {
+  DEBUG_CMDS,
+
+  &CMDINFO_TEST0,
+  nullptr
+};
+
+
+
+int main(void)
+{
+  BOARD_PROLOG;
+
+  UVAR('t') = 10; // 10 ms
+  UVAR('v') = v_adc_ref;
+  UVAR('c') = n_ADC_ch_max;
+  UVAR('n') = 20; // number of series
+  UVAR('s') = 6; // sampling time index
+
+  #ifdef PWR_CR1_ADCDC1
+  PWR->CR1 |= PWR_CR1_ADCDC1;
+  #endif
+
+  BOARD_POST_INIT_BLINK;
+
+  pr( NL "##################### " PROJ_NAME NL );
+
+  srl.re_ps();
+
+  oxc_add_aux_tick_fun( led_task_nortos );
+
+  std_main_loop_nortos( &srl, nullptr );
+
+  return 0;
+}
+
+
+
+// TEST0
+int cmd_test0( int argc, const char * const * argv )
+{
+  STDOUT_os;
+  char pbuf[pbufsz];
+  int t_step = UVAR('t');
+  uint8_t n_ch = UVAR('c');
+  if( n_ch > n_ADC_ch_max ) { n_ch = n_ADC_ch_max; };
+  if( n_ch < 1 ) { n_ch = 1; };
+
+  uint32_t n = arg2long_d( 1, argc, argv, UVAR('n'), 1, 1000000 ); // number of series
+
+  uint32_t sampl_t_idx = UVAR('s');
+  if( sampl_t_idx >= adc_n_sampl_times ) { sampl_t_idx = adc_n_sampl_times-1; };
+  uint32_t f_sampl_max = adc.adc_clk / ( sampl_times_cycles[sampl_t_idx] * n_ch );
+
+  uint32_t adc_presc = hint_ADC_presc();
+  UVAR('i') =  adc_init_exa_4ch_manual( adc, adc_presc, sampl_times_codes[sampl_t_idx], n_ch );
+  delay_ms( 1 );
+  if( ! UVAR('i') ) {
+    os <<  "ADC init failed, errno= " << errno << NL;
+    return 1;
+  }
+  if( UVAR('d') > 1 ) { pr_ADC_state( adc );  }
+
+
+  int div_val = -1;
+  adc.adc_clk = calc_ADC_clk( adc_presc, &div_val );
+  snprintf( pbuf, pbufsz-1, "# ADC: n_ch= %d n=%lu adc_clk= %lu  div_val= %d s_idx= %lu sampl= %lu; f_sampl_max= %lu Hz" NL,
+                                    n_ch,    n,    adc.adc_clk,  div_val,   sampl_t_idx, sampl_times_cycles[sampl_t_idx],
+                                    f_sampl_max );
+  os << pbuf;
+  delay_ms( 10 );
+
+  uint32_t n_ADC_sampl = n_ch;
+
+  adc.reset_cnt();
+
+  leds.reset( BIT0 | BIT1 | BIT2 );
+
+  uint32_t tm0, tm00;
+  int rc = 0;
+
+  for( unsigned i=0; i<n && !break_flag; ++i ) {
+    if( HAL_ADC_Start_DMA( &adc.hadc, (uint32_t*)(&ADC_buf), n_ADC_sampl ) != HAL_OK )   {
+      os <<  "ADC_Start_DMA error" NL;
+      rc = 1;
+      break;
+    }
+
+    for( uint32_t ti=0; adc.end_dma == 0 && ti<100; ++ti ) {
+      delay_ms(1);
+    }
+    uint32_t tcc = HAL_GetTick();
+    if( i == 0 ) {
+      tm0 = HAL_GetTick(); tm00 = tm0;
+    }
+
+    HAL_ADC_Stop_DMA( &adc.hadc ); // needed
+    if( adc.end_dma == 0 ) {
+      os <<  "Fail to wait DMA end " NL;
+      rc = 2;
+      break;
+    }
+    if( adc.dma_error != 0 ) {
+      os <<  "Found DMA error " << HexInt( adc.dma_error ) <<  NL;
+      rc = 3;
+      break;
+    } else {
+      adc.n_series = 1;
+    }
+    os <<  0.001f * ( tcc - tm00 )  << ' ';
+    for( int j=0; j<n_ch; ++j ) {
+      os << ' ' << ( 0.001f * UVAR('v')  * ADC_buf[j] / 4096 );
+    }
+    os  <<  NL;
+
+    delay_ms_until_brk( &tm0, t_step );
+  }
+
+
+  delay_ms( 10 );
+
+  return rc;
+}
+
+
+
+
+void HAL_ADC_ConvCpltCallback( ADC_HandleTypeDef *hadc )
+{
+  adc.end_dma |= 1;
+  adc.good_SR =  adc.last_SR = adc.hadc.Instance->SR;
+  adc.last_end = 1;
+  adc.last_error = 0;
+  ++adc.n_good;
+}
+
+void HAL_ADC_ErrorCallback( ADC_HandleTypeDef *hadc )
+{
+  adc.end_dma |= 2;
+  adc.bad_SR = adc.last_SR = adc.hadc.Instance->SR;
+  // tim2_deinit();
+  adc.last_end  = 2;
+  adc.last_error = HAL_ADC_GetError( hadc );
+  adc.dma_error = hadc->DMA_Handle->ErrorCode;
+  hadc->DMA_Handle->ErrorCode = 0;
+  ++adc.n_bad;
+}
+
+void DMA2_Stream0_IRQHandler(void)
+{
+  HAL_DMA_IRQHandler( &adc.hdma_adc );
+}
+
+// vim: path=.,/usr/share/stm32cube/inc/,/usr/arm-none-eabi/include,/usr/share/stm32oxc/inc
+
