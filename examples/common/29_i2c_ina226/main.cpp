75b4b13a0c4b16f7f81c8ae9a3407a204b6ccc64
Anton Guda <atu@nmetau.edu.ua>
2019-01-16 20:05:26 +0200

    Add calibration to 29_i2c_ina226

--- examples/common/29_i2c_ina226/main.cpp
+++ examples/common/29_i2c_ina226/main.cpp
@@ -22,7 +22,9 @@ const char* common_help_string = "App to test INA226 I2C device" NL;
 int cmd_test0( int argc, const char * const * argv );
 CmdInfo CMDINFO_TEST0 { "test0", 'T', cmd_test0, " - test V_sh, V_bus"  };
 int cmd_getVIP( int argc, const char * const * argv );
-CmdInfo CMDINFO_GETNCH { "getVIP", 'G', cmd_getVIP, " - get V_bus, I_sh, P"  };
+CmdInfo CMDINFO_GETVIP { "getVIP", 'G', cmd_getVIP, " - get V_bus, I_sh, P"  };
+int cmd_setcalibr( int argc, const char * const * argv );
+CmdInfo CMDINFO_SETCALIBR { "set_calibr", 'K', cmd_setcalibr, " I_lsb R_sh - calibrate for given shunt"  };
 int cmd_setaddr( int argc, const char * const * argv );
 CmdInfo CMDINFO_SETADDR { "setaddr", 0, cmd_setaddr, " addr - set device addr"  };
 
@@ -31,7 +33,8 @@ const CmdInfo* global_cmds[] = {
   DEBUG_I2C_CMDS,
 
   &CMDINFO_TEST0,
-  &CMDINFO_GETNCH,
+  &CMDINFO_GETVIP,
+  &CMDINFO_SETCALIBR,
   &CMDINFO_SETADDR,
   nullptr
 };
@@ -40,6 +43,8 @@ const CmdInfo* global_cmds[] = {
 I2C_HandleTypeDef i2ch;
 DevI2C i2cd( &i2ch, 0 );
 INA226 ina226( i2cd );
+double calibr_I_lsb = 0.001;  // 1 mA = 1 bit
+double calibr_R     = 0.1175; // shunt Ohm
 
 bool isGoodINA226( INA226 &ina, bool print = true );
 
@@ -152,9 +157,87 @@ int cmd_test0( int argc, const char * const * argv )
 
 int cmd_getVIP( int argc, const char * const * argv )
 {
-  // int n = arg2long_d( 1, argc, argv, UVAR('n'), 0 );
-  // uint32_t t_step = UVAR('t');
+  STDOUT_os;
+  int t_step = UVAR('t');
+  uint32_t n = arg2long_d( 1, argc, argv, UVAR('n'), 1, 1000000 ); // number of series
+  unsigned n_ch = 3;
 
+  ina226.setCfg( INA226::cfg_rst );
+  uint16_t x_cfg = ina226.getCfg();
+  os <<  NL "# Test0: n= " <<  n <<  " t= " <<  t_step <<  "  cfg= " <<  HexInt16( x_cfg ) << NL;
+
+  if( ! isGoodINA226( ina226, true ) ) {
+    return 3;
+  }
+
+  uint16_t cfg = INA226::cfg_default;
+  UVAR('e') = ina226.setCfg( cfg );
+  x_cfg = ina226.getCfg();
+  os <<  "# cfg= " << HexInt16( x_cfg ) <<  NL;
+  ina226.setCalibr( (uint16_t)( 0.00512 / ( calibr_I_lsb * calibr_R ) ) ); // 1 mA 0.1xxx Ohm
+
+  StatData sdat( n_ch );
+
+  leds.set(   BIT0 | BIT1 | BIT2 ); delay_ms( 100 );
+  leds.reset( BIT0 | BIT1 | BIT2 );
+
+  uint32_t tm0, tm00;
+  int rc = 0;
+  bool do_out = ! UVAR('b');
+
+  break_flag = 0;
+  for( unsigned i=0; i<n && !break_flag; ++i ) {
+
+    uint32_t tcc = HAL_GetTick();
+    if( i == 0 ) {
+      tm0 = tcc; tm00 = tm0;
+    }
+
+    if( UVAR('l') ) {  leds.set( BIT2 ); }
+    // int16_t v_sh_raw  = ina226.getVsh();
+    int16_t v_bus_raw = ina226.getVbus();
+    int16_t I_raw     = ina226.getI();
+    int16_t P_raw     = ina226.getP();
+    if( UVAR('l') ) {  leds.reset( BIT2 ); }
+
+    double v[n_ch];
+    // v[0] = INA226::lsb_V_sh_nv  * v_sh_raw  * 1e-9;
+    v[0] = INA226::lsb_V_bus_nv * v_bus_raw * 1e-9;
+    v[1] = I_raw * calibr_I_lsb;
+    v[2] = P_raw;
+
+    int dt = tcc - tm00; // ms
+    if( do_out ) {
+      os <<  FloatFmt( 0.001 * dt, "%-10.4f "  );
+    }
+
+    sdat.add( v );
+
+    if( do_out ) {
+      for( auto vc : v ) {
+        os  << ' '  <<  FloatFmt( vc, "%#12.6g" );
+      }
+    }
+    os << NL;
+
+    delay_ms_until_brk( &tm0, t_step );
+  }
+
+  sdat.calc();
+  os << sdat << NL;
+
+  return rc;
+}
+
+int cmd_setcalibr( int argc, const char * const * argv )
+{
+  calibr_I_lsb = arg2float_d( 1, argc, argv, calibr_I_lsb, 1e-20, 1e10 );
+  calibr_R     = arg2float_d( 2, argc, argv,     calibr_R, 1e-20, 1e10 );
+  double V_sh_max =  INA226::lsb_V_sh_nv * 1e-9 * 0x7FFF;
+  STDOUT_os;
+  os << "# calibr_I_lsb= " << calibr_I_lsb << " calibr_R= " << calibr_R
+     << " V_sh_max=  " << V_sh_max
+     << " I_max= " << ( V_sh_max / calibr_R ) << " / " << ( calibr_I_lsb * 0x7FFF ) << NL;
   return 0;
 }
 
