25ffe593db5011a5f4e29923f20a93cb2d49b850
Anton Guda <atu@nmetau.edu.ua>
2019-07-06 17:34:32 +0300

    Make init struct const in stm32f334_clock_HSE072.c

--- src/stm32f334_clock_HSE072.c
+++ src/stm32f334_clock_HSE072.c
@@ -14,48 +14,54 @@ void  approx_delay_calibrate(void);
 
 int SystemClockCfg(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_OscInitStruct.OscillatorType      = RCC_OSCILLATORTYPE_HSI | RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState            = RCC_HSE_BYPASS;
-  RCC_OscInitStruct.HSEPredivValue      = RCC_HSE_PREDIV_DIV1;
-  RCC_OscInitStruct.HSIState            = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState        = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource       = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLMUL          = RCC_PLL_MUL9;
-  if( HAL_RCC_OscConfig( &RCC_OscInitStruct ) != HAL_OK ) {
+  static const RCC_OscInitTypeDef RCC_OscInitStruct = {
+    .OscillatorType      = RCC_OSCILLATORTYPE_HSI | RCC_OSCILLATORTYPE_HSE,
+    .HSEState            = RCC_HSE_BYPASS,
+    .HSEPredivValue      = RCC_HSE_PREDIV_DIV1,
+    .HSIState            = RCC_HSI_ON,
+    .HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT,
+    .PLL.PLLState        = RCC_PLL_ON,
+    .PLL.PLLSource       = RCC_PLLSOURCE_HSE,
+    .PLL.PLLMUL          = RCC_PLL_MUL9
+  };
+
+  if( HAL_RCC_OscConfig( (RCC_OscInitTypeDef*)( &RCC_OscInitStruct ) ) != HAL_OK ) {
     errno = 1001;
     return  1001;
   }
 
-  RCC_ClkInitTypeDef RCC_ClkInitStruct;
-  RCC_ClkInitStruct.ClockType =
-      RCC_CLOCKTYPE_SYSCLK
-    | RCC_CLOCKTYPE_HCLK
-    | RCC_CLOCKTYPE_PCLK1
-    | RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider  = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-  if( HAL_RCC_ClockConfig( &RCC_ClkInitStruct, FLASH_LATENCY_2 ) != HAL_OK ) {
+  static const RCC_ClkInitTypeDef RCC_ClkInitStruct = {
+    .ClockType =
+        RCC_CLOCKTYPE_SYSCLK
+      | RCC_CLOCKTYPE_HCLK
+      | RCC_CLOCKTYPE_PCLK1
+      | RCC_CLOCKTYPE_PCLK2,
+    .SYSCLKSource   = RCC_SYSCLKSOURCE_PLLCLK,
+    .AHBCLKDivider  = RCC_SYSCLK_DIV1,
+    .APB1CLKDivider = RCC_HCLK_DIV2,
+    .APB2CLKDivider = RCC_HCLK_DIV1
+  };
+
+  if( HAL_RCC_ClockConfig( (RCC_ClkInitTypeDef*)( &RCC_ClkInitStruct ), FLASH_LATENCY_2 ) != HAL_OK ) {
     errno = 1003;
     return  1003;
   }
 
-  RCC_PeriphCLKInitTypeDef PeriphClkInit;
-  PeriphClkInit.PeriphClockSelection =
-      RCC_PERIPHCLK_HRTIM1
-    | RCC_PERIPHCLK_USART1
-    | RCC_PERIPHCLK_I2C1
-    | RCC_PERIPHCLK_TIM1
-    | RCC_PERIPHCLK_ADC12;
-  PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_SYSCLK;
-  PeriphClkInit.Adc12ClockSelection  = RCC_ADC12PLLCLK_DIV1;
-  PeriphClkInit.I2c1ClockSelection   = RCC_I2C1CLKSOURCE_SYSCLK;
-  PeriphClkInit.Tim1ClockSelection   = RCC_TIM1CLK_PLLCLK;
-  PeriphClkInit.Hrtim1ClockSelection = RCC_HRTIM1CLK_PLLCLK;
-  if( HAL_RCCEx_PeriphCLKConfig( &PeriphClkInit ) != HAL_OK ) {
+  static const RCC_PeriphCLKInitTypeDef PeriphClkInit = {
+    .PeriphClockSelection =
+        RCC_PERIPHCLK_HRTIM1
+      | RCC_PERIPHCLK_USART1
+      | RCC_PERIPHCLK_I2C1
+      | RCC_PERIPHCLK_TIM1
+      | RCC_PERIPHCLK_ADC12,
+    .Usart1ClockSelection = RCC_USART1CLKSOURCE_SYSCLK,
+    .Adc12ClockSelection  = RCC_ADC12PLLCLK_DIV1,
+    .I2c1ClockSelection   = RCC_I2C1CLKSOURCE_SYSCLK,
+    .Tim1ClockSelection   = RCC_TIM1CLK_PLLCLK,
+    .Hrtim1ClockSelection = RCC_HRTIM1CLK_PLLCLK
+  };
+
+  if( HAL_RCCEx_PeriphCLKConfig( (RCC_PeriphCLKInitTypeDef*)( &PeriphClkInit ) ) != HAL_OK ) {
     errno = 1004;
     return  1004;
   }
