33089584a6b5a69e49ff544ee13d49a1a3a3368c
Anton Guda <atu@nmetau.edu.ua>
2020-07-21 15:45:56 +0300

    Fix size in 12d_tim_ds2812

--- examples/common/12d_tim_ds2812/main.cpp
+++ examples/common/12d_tim_ds2812/main.cpp
@@ -19,7 +19,7 @@ TIM_HandleTypeDef tim_h;
 DMA_HandleTypeDef hdma_tim_ch1;
 
 // debug
-PinOut dbg_pin( GpioA, 9 );
+// PinOut dbg_pin( GpioA, 9 );
 char xlog_buf[2048];
 
 
@@ -40,7 +40,8 @@ struct DS2812_info {
   uint16_t buf[ 2 * size_1led ]; // 2 chunks, each 24: 1 bit (CCR) for output via timer
   uint8_t *ibuf = nullptr;
   int size = 0;
-  volatile int pos = -2;      // pos not unsigned - special values for start/stop
+  volatile int pos = 0;
+  volatile bool busy = false;
   int send( const uint8_t *d, int sz );
   static void rgb2tim( uint8_t r, uint8_t g, uint8_t b, uint16_t *d );
   static void color2tim( uint8_t c, uint16_t *d );
@@ -53,7 +54,7 @@ uint16_t DS2812_info::t_min, DS2812_info::t_max;
 
 void DS2812_info::calc_minmax( uint16_t arr )
 {
-  t_min = (uint16_t)( arr * 8 / 25 ); // * 0.32
+  t_min = (uint16_t)( arr * 7 / 25 ); // * 0.35/1.25  = 0.28
   t_max = (uint16_t)( arr - t_min );
 }
 
@@ -75,16 +76,32 @@ void DS2812_info::rgb2tim( uint8_t r, uint8_t g, uint8_t b, uint16_t *d )
   color2tim( b, d+16 );
 }
 
-int DS2812_info::send( const uint8_t *d, int sz )
+int DS2812_info::send( const uint8_t *d, int sz ) // size in leds: 3*sz bytes in d[] required
 {
   sz &= ~1; // TODO: may be better:
   if( size != 0 || !d || sz < 2 ) {
     return 0;
   }
-  size = sz; pos = -2;
+  size = sz; pos = 0;
+  busy = true;
+  unsigned tw_ms = 2 + ( 1250 * sz + 120000 ) / 1000000;
+  int rc_s = HAL_TIM_PWM_Start_DMA( &tim_h, TIM_CHANNEL_1, (uint32_t*)buf, 2 * size_1led );
+  if( rc_s != HAL_OK ) {
+    size = 0;  busy = false;
+    return 0;
+  }
+
+  for( unsigned i=0; i<tw_ms; ++i ) {
+    delay_ms( 1 );
+    if( !busy ) {
+      break;
+    }
+  }
+  rc_s = !busy;
 
   size = 0;
-  return 1;
+  busy = false;
+  return rc_s;
 }
 
 DS2812_info dsi;
@@ -107,14 +124,14 @@ int main(void)
   BOARD_PROLOG;
 
   UVAR('t') = 5;
-  UVAR('n') = 1;
+  UVAR('n') = 4;
 
   BOARD_POST_INIT_BLINK;
 
   pr( NL "##################### " PROJ_NAME NL );
 
-  dbg_pin.initHW();
-  dbg_pin.reset();
+  // dbg_pin.initHW();
+  // dbg_pin.reset();
   MX_DMA_Init();
   tim_cfg();
   set_log_buf( xlog_buf, sizeof(xlog_buf) );
@@ -132,54 +149,40 @@ int main(void)
 // TEST0
 int cmd_test0( int argc, const char * const * argv )
 {
-  // unsigned v = arg2long_d( 1, argc, argv, 0, 0, 100 );
+  unsigned v = arg2long_d( 1, argc, argv, 32, 0, 255 );
   unsigned td = arg2long_d( 2, argc, argv, UVAR('t'), 0 );
-  // unsigned n = UVAR('n');
+  unsigned n = UVAR('n');
 
 
   std_out << "# Test:  " <<  "  td= " << td <<  NL;
   std_out << "## cb: " << HexInt((void*)hdma_tim_ch1.XferCpltCallback) << NL;
 
-  lbuf[0] = 0xFF; lbuf[1]  = 0x00; lbuf[2]  = 0x00;
-  lbuf[3] = 0x00; lbuf[4]  = 0xFF; lbuf[5]  = 0x00;
-  lbuf[6] = 0x00; lbuf[7]  = 0x00; lbuf[8]  = 0xFF;
-  lbuf[9] = 0x20; lbuf[10] = 0x20; lbuf[11] = 0x20;
-  dsi.ibuf = lbuf; dsi.size = 4; dsi.pos = 0;
+  for( unsigned i=0; i<n*3; ++i ) {
+    lbuf[i] = 1;
+  }
+
+  lbuf[0] =    v; lbuf[1]  = 0x00; lbuf[2]  = 0x00;
+  lbuf[3] = 0x00; lbuf[4]  =    v; lbuf[5]  = 0x00;
+  lbuf[6] = 0x00; lbuf[7]  = 0x00; lbuf[8]  =    v;
+  lbuf[9] =    v; lbuf[10] =    v; lbuf[11] =    v;
+  dsi.ibuf = lbuf; dsi.size = n; dsi.pos = 0;
   dsi.fill_zeros();
 
   log_reset();
 
-  // if( v > 0 ) {
-  //   dsi.fill_zeros();
-  // } else {
-  //   DS2812_info::rgb2tim( 0x55, 0x33, 0x11, dsi.buf );
-  //   DS2812_info::rgb2tim( 0xAA, 0x55, 0x77, dsi.buf+24 );
-  // }
 
-  dbg_pin.set();
-  // __HAL_TIM_ENABLE( &tim_h );
-  //TIM_CCxChannelCmd( TIM_EXA, TIM_CHANNEL_1, 0 );
+  // dbg_pin.set();
   delay_mcs( 20 );
   UVAR('i') = 0;
   UVAR('j') = 0;
   UVAR('k') = 0;
   UVAR('q') = 0;
 
-  UVAR('r') = HAL_TIM_PWM_Start_DMA( &tim_h, TIM_CHANNEL_1, (uint32_t*)dsi.buf, 2*24 );
-
-  // tim_print_cfg( TIM_EXA );
-  // dump32( TIM_EXA, 0x100, 1 );
+  UVAR('r') = HAL_TIM_PWM_Start_DMA( &tim_h, TIM_CHANNEL_1, (uint32_t*)dsi.buf, 2 * DS2812_info::size_1led );
 
   delay_ms( td );
-  HAL_TIM_PWM_Stop_DMA( &tim_h, TIM_CHANNEL_1 );
-  TIM_EXA->CCR1 = 0;
-  TIM_EXA->CNT = 0;
 
-  // delay_ms( td );
-  delay_mcs( 20 );
-  // TIM_CCxChannelCmd( TIM_EXA, TIM_CHANNEL_1, 0 );
-  // __HAL_TIM_DISABLE( &tim_h );
-  dbg_pin.reset();
+  // dbg_pin.reset();
 
 
   // uint32_t tm0 = HAL_GetTick();
@@ -237,9 +240,6 @@ void tim_cfg()
     return;
   }
 
-  TIM_EXA->CCR1 = 0;
-  TIM_EXA->CNT  = 0;
-  // TIM_CCxChannelCmd( TIM_EXA, TIM_CHANNEL_1, 0 );
   UVAR('x') = 11;
 
 }
@@ -262,7 +262,6 @@ void HAL_TIM_PWM_MspInit( TIM_HandleTypeDef* htim )
   hdma_tim_ch1.Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD;
   hdma_tim_ch1.Init.MemDataAlignment    = DMA_MDATAALIGN_HALFWORD;
   hdma_tim_ch1.Init.Mode                = DMA_CIRCULAR;
-  // hdma_tim_ch1.Init.Mode                = DMA_NORMAL;
   hdma_tim_ch1.Init.Priority            = DMA_PRIORITY_MEDIUM;
   hdma_tim_ch1.Init.FIFOMode            = DMA_FIFOMODE_DISABLE;
   if( HAL_DMA_Init( &hdma_tim_ch1 ) != HAL_OK ) {
@@ -287,7 +286,6 @@ void HAL_TIM_PWM_MspDeInit( TIM_HandleTypeDef* htim )
   }
   TIM_EXA_CLKDIS;
   TIM_EXA_GPIO.cfgIn_N( TIM_EXA_PINS );
-  // HAL_NVIC_DisableIRQ( TIM_EXA_IRQ );
 }
 
 void MX_DMA_Init()
@@ -343,25 +341,32 @@ void DMA2_Stream1_IRQHandler()
   HAL_DMA_IRQHandler( &hdma_tim_ch1 );
 
 
-  if( p > dsi.size+2 ) {
-    log_add( "-STOP-" );
+  if( p > dsi.size+1 ) {
+    log_add( "-X-" );
     HAL_TIM_PWM_Stop_DMA( &tim_h, TIM_CHANNEL_1 );
     // __HAL_TIM_DISABLE( &tim_h );
+  }
+  if( p > dsi.size+2 ) {
+    log_add( "-Z-" );
+    dsi.busy = false;
   }
 
   if( hc ) {
     if( p < dsi.size ) {
       DS2812_info::rgb2tim( dsi.ibuf[xpos], dsi.ibuf[xpos+1], dsi.ibuf[xpos+2], dsi.buf );
+      log_add( "0" );
     } else {
       dsi.fill_zeros0();
+      log_add( "z0" );
     }
   }
-
-  if( tc ) {
+  else if( tc ) {
     if( p < dsi.size ) {
       DS2812_info::rgb2tim( dsi.ibuf[xpos], dsi.ibuf[xpos+1], dsi.ibuf[xpos+2], dsi.buf+24 );
+      log_add( "1" );
     } else {
       dsi.fill_zeros1();
+      log_add( "z1" );
     }
   }
 
