4f99370000353cb59764188b43da1514df38e02c
Anton Guda <atu@nmetau.edu.ua>
2022-07-20 12:24:39 +0300

    Add StepMotor
    In example 16 for now

--- examples/common/16_step1/main.cpp
+++ examples/common/16_step1/main.cpp
@@ -1,3 +1,5 @@
+// #include <ranges>
+
 #include <oxc_auto.h>
 
 using namespace std;
@@ -8,11 +10,90 @@ BOARD_DEFINE_LEDS;
 
 BOARD_CONSOLE_DEFINES;
 
+class StepMotorDriverBase {
+  public:
+   virtual void set( uint8_t outs ) noexcept = 0;
+   virtual void init() noexcept = 0;
+};
+
+class StepMotorDriverGPIO : public StepMotorDriverBase {
+  public:
+   explicit StepMotorDriverGPIO( PinsOut &a_pins ) noexcept : pins( a_pins ) {}
+   virtual void set( uint8_t outs ) noexcept override { pins.write( outs ); };
+   virtual void init() noexcept override { pins.initHW(); };
+  protected:
+   PinsOut &pins;
+};
+
+class StepMotor
+{
+  public:
+   struct MotorMode {
+     std::size_t n_steps;
+     const uint8_t *steps;
+   };
+   StepMotor( StepMotorDriverBase &a_drv, std::size_t mode ) noexcept
+     : drv( a_drv )
+     { setMode( mode ); };
+   // void set( uint8_t v ) noexcept { drv.set( v ); }
+   void off() noexcept { drv.set( 0 ); }
+   void init() noexcept { drv.init(); ph = 0; }
+   void setMode( size_t a_mode ) noexcept;
+   size_t getMode() const noexcept { return mode; }
+   void setExternMode( const uint8_t *st, size_t ns ) noexcept;
+   size_t getPhase() const noexcept { return ph; }
+   size_t getV() const noexcept { return steps[ph]; }
+   void setPhase( size_t phase ) noexcept { ph = phase % n_steps; }
+   void stepF() { return step(  1 ); }
+   void stepB() { return step( -1 ); };
+   void step( int v );
+  protected:
+   StepMotorDriverBase &drv;
+   const uint8_t *steps { nullptr };
+   size_t n_steps { 0 };
+   size_t ph { 0 };
+   size_t mode { 0 };
+   static constexpr uint8_t half_steps4[] { 1, 3, 2, 6, 4, 12, 8, 9 };
+   static constexpr uint8_t full_steps4[] { 1, 2, 4, 8 };
+   static constexpr uint8_t half_steps3[] { 1, 3, 2, 6, 4, 5 };
+   static constexpr uint8_t full_steps3[] { 1, 2, 4 };
+   static constexpr MotorMode m_modes[] = {
+     { size(full_steps4), full_steps4 },
+     { size(half_steps4), half_steps4 },
+     { size(half_steps3), half_steps3 },
+     { size(half_steps3), full_steps3 }
+   };
+   static constexpr size_t n_modes = size(m_modes);
+};
+
+void StepMotor::setMode( size_t a_mode ) noexcept
+{
+  mode    = (mode<n_modes)? a_mode : 0;
+  steps   = m_modes[mode].steps;
+  n_steps = m_modes[mode].n_steps;
+  ph = 0;
+}
+
+void StepMotor::setExternMode( const uint8_t *st, size_t ns ) noexcept
+{
+  steps   = st;
+  n_steps = ns;
+  ph = 0;
+  mode = 0xFF; // fake
+}
+
+void StepMotor::step( int v )
+{
+  ph += (size_t)v; // TODO: check negative
+  ph %= n_steps;
+  drv.set( steps[ph] );
+}
+
 const char* common_help_string = "App to test stepmotor" NL;
 
 // --- local commands;
 int cmd_test0( int argc, const char * const * argv );
-CmdInfo CMDINFO_TEST0 { "test0", 'T', cmd_test0, " [N] [01] - test step 0"  };
+CmdInfo CMDINFO_TEST0 { "test0", 'T', cmd_test0, " [N]  - test step"  };
 
 const CmdInfo* global_cmds[] = {
   DEBUG_CMDS,
@@ -22,23 +103,9 @@ const CmdInfo* global_cmds[] = {
 };
 
 PinsOut motor { BOARD_MOTOR_DEFAULT_GPIO, BOARD_MOTOR_DEFAULT_PIN0, 4 };
+StepMotorDriverGPIO m_drv( motor );
+StepMotor mot( m_drv, 0 );
 
-const uint8_t half_steps4[] = { 1, 3, 2, 6, 4, 12, 8, 9 };
-const uint8_t full_steps4[] = { 1, 2, 4, 8 };
-const uint8_t half_steps3[] = { 1, 3, 2, 6, 4, 5 };
-
-struct MotorMode {
-  int n_steps;
-  const uint8_t *steps;
-};
-
-MotorMode m_modes[] = {
-  { 8, half_steps4 },
-  { 4, full_steps4 },
-  { 6, half_steps3 }, // 3-phase modes
-  { 3, full_steps4 }  // part of 4-phase
-};
-const int n_modes = sizeof(m_modes)/sizeof(MotorMode);
 
 
 int main(void)
@@ -47,8 +114,10 @@ int main(void)
 
   UVAR('t') = 1000;
   UVAR('n') = 20;
+  UVAR('a') = 1; // autoOff
 
-  motor.initHW();
+
+  mot.init();
 
   BOARD_POST_INIT_BLINK;
 
@@ -67,48 +136,37 @@ int main(void)
 // TEST0
 int cmd_test0( int argc, const char * const * argv )
 {
-  static int ph = 0;
-  int n = arg2long_d( 1, argc, argv, UVAR('n'), 0 );
+  int n = arg2long_d( 1, argc, argv, UVAR('n')  );
   uint32_t t_step = UVAR('t');
 
-  int m = UVAR('m');
-  if( m >= n_modes ) {
-    m = 0;
+  auto m = mot.getMode();
+  if( (int)m != UVAR('m') ) {
+    mot.setMode( UVAR('m') );
   }
+  m = mot.getMode();
+  int d  = ( n >= 0 ) ? 1 : -1;
+  int nn = ( n >= 0 ) ? n : -n;
 
-  const uint8_t *steps = m_modes[m].steps;
-  int ns = m_modes[m].n_steps;
-  if( ph >= ns ||  ph < 0 ) { ph = 0; }
-
-  int d = 1;
-  if( argc > 2  && argv[2][0] == '-' ) {
-    d = ns - 1; // % no zero
-  }
-  std_out << NL "Test0: n= " << n << " t= " << t_step << " m= "  << m << " d= "  << d <<  NL;
-
-  if( n < 1 ) {
-    motor.write( 0 );
-    ph = 0;
-    std_out <<  NL "Motor off." NL;
-    return 0;
-  }
+  std_out << NL "Test0: n= " << n << " t= " << t_step << " m= "  << m << " d= " << d << NL;
 
 
   uint32_t tm0 = HAL_GetTick();
 
   break_flag = 0;
-  for( int i=0; i<n && !break_flag; ++i ) {
+  for( int i=0; i<nn && !break_flag; ++i ) {
 
+    mot.step( d );
     if( t_step > 500 ) {
-      std_out <<  " Step  i= " <<  i <<  " ph: "  <<  ph  <<  " v: "  <<  steps[ph]
-         <<  "  tick: " << ( HAL_GetTick() - tm0 )   <<  NL;
+      std_out <<  i <<  ' '  <<  mot.getPhase()  <<  ' '  <<  mot.getV()
+         <<  ' ' << ( HAL_GetTick() - tm0 )   <<  NL;
       std_out.flush();
     }
-    motor.write( steps[ph] );
-    ph += d;
-    ph %= ns;
 
     delay_ms_until_brk( &tm0, t_step );
+  }
+
+  if( UVAR('o') ) {
+    mot.off();
   }
 
   return 0;
