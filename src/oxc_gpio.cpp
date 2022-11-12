3c44aec537e0e71fbd1d62868245b8ef06f5bc0d
Anton Guda <atu@nmetau.edu.ua>
2022-11-12 20:34:39 +0200

    Remove unused GPIO_enableClk function

--- src/oxc_gpio.cpp
+++ src/oxc_gpio.cpp
@@ -120,62 +120,6 @@ void GpioRegs::setEXTI( uint8_t pin, ExtiEv ev )
 
 // ********************************************************
 
-void GPIO_enableClk( GPIO_TypeDef* gp )
-{
-  if( gp == GPIOA ) {
-    __GPIOA_CLK_ENABLE();
-    return;
-  }
-  if( gp == GPIOB ) {
-    __GPIOB_CLK_ENABLE();
-    return;
-  }
-  if( gp == GPIOC ) {
-    __GPIOC_CLK_ENABLE();
-    return;
-  }
-  if( gp == GPIOD ) {
-    __GPIOD_CLK_ENABLE();
-    return;
-  }
-  #ifdef GPIOE
-  if( gp == GPIOE ) {
-    __GPIOE_CLK_ENABLE();
-    return;
-  }
-  #endif
-  #ifdef GPIOF
-  if( gp == GPIOF ) {
-    __GPIOF_CLK_ENABLE();
-    return;
-  }
-  #endif
-  #ifdef GPIOG
-  if( gp == GPIOG ) {
-    __GPIOG_CLK_ENABLE();
-    return;
-  }
-  #endif
-  #ifdef GPIOH
-  if( gp == GPIOH ) {
-    __GPIOH_CLK_ENABLE();
-    return;
-  }
-  #endif
-  #ifdef GPIOI
-  if( gp == GPIOI ) {
-    __GPIOI_CLK_ENABLE();
-    return;
-  }
-  #endif
-  #ifdef GPIOJ
-  if( gp == GPIOJ ) {
-    __GPIOJ_CLK_ENABLE();
-    return;
-  }
-  #endif
-}
-
 
 
 void GPIO_WriteBits( GPIO_TypeDef* GPIOx, uint16_t PortVal, uint16_t mask )
