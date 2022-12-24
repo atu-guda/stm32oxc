4e27a6ab2123e4b4e8ed703a9372bfa9d8d17bb4
Anton Guda <atu@nmetau.edu.ua>
2022-12-24 14:39:33 +0200

    Test special reg handling in t01_misc

--- examples/f446_nucleo64/t01_misc/main.cpp
+++ examples/f446_nucleo64/t01_misc/main.cpp
@@ -21,14 +21,20 @@ const CmdInfo* global_cmds[] = {
   nullptr
 };
 
+volatile uint32_t ixpsr {0};
 
+
+void aux_tick_fun2(void)
+{
+  ixpsr = __get_xPSR();
+}
 
 
 int main(void)
 {
   STD_PROLOG_UART;
 
-  UVAR('t') = 1000;
+  UVAR('t') = 100;
   UVAR('n') = 10;
 
   BOARD_POST_INIT_BLINK;
@@ -38,6 +44,7 @@ int main(void)
   srl.re_ps();
 
   oxc_add_aux_tick_fun( led_task_nortos );
+  oxc_add_aux_tick_fun( aux_tick_fun2 );
 
   std_main_loop_nortos( &srl, nullptr );
 
@@ -50,6 +57,15 @@ int cmd_test0( int argc, const char * const * argv )
 {
   int n = arg2long_d( 1, argc, argv, UVAR('n'), 0 );
   uint32_t t_step = UVAR('t');
+
+  std_out
+    << "# ctrl= "      << HexInt(__get_CONTROL())
+    << " apsr= "       << HexInt(__get_APSR())
+    << " ipsr= "       << HexInt(__get_IPSR())
+    << " xpsr= "       << HexInt(__get_xPSR())
+    << " ipsr= "       << HexInt(ixpsr)
+    << " primask= "    << HexInt(__get_PRIMASK())
+    << NL;
 
 
   uint32_t tm0 = HAL_GetTick(), tm00 = tm0;
