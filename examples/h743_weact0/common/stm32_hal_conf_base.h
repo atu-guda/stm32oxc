d89f479b00480fd7522663a97982c228fbcde19d
Anton Guda <atu@nmetau.edu.ua>
2024-10-22 20:12:31 +0300

    First run on h743_weact0

---
+++ examples/h743_weact0/common/stm32_hal_conf_base.h
@@ -0,0 +1,13 @@
+#ifndef __STM32_HAL_CONF_H
+#define __STM32_HAL_CONF_H
+
+// base configuaration file for oxc h743 waveshare board examples
+
+#define REQ_MCBASE STM32H7
+#ifndef REQ_SYSCLK_FREQ
+  #define REQ_SYSCLK_FREQ 400
+#endif
+
+
+#endif /* __STM32_HAL_CONF_H */
+
