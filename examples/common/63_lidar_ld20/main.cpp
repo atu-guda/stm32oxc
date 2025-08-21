c53844817feaba1fe6fe68c568ba9cab3e83e21d
Anton Guda <atu@nmetau.edu.ua>
2025-08-21 20:13:54 +0300

    Use HAL_UART_Receive_DMA 63_lidar_ld20, w/o Idle

--- examples/common/63_lidar_ld20/main.cpp
+++ examples/common/63_lidar_ld20/main.cpp
@@ -81,75 +81,52 @@ int main(void)
   return 0;
 }
 
-
-void HAL_UARTEx_RxEventCallback( UART_HandleTypeDef *huart, uint16_t sz )
+void HAL_UART_RxCpltCallback( UART_HandleTypeDef *huart )
 {
-  if( huart->Instance != UART_LIDAR_LD20 ) {
+  leds.set( 4 );
+  if( !huart || huart->Instance != UART_LIDAR_LD20 ) {
     return;
   }
   ++UVAR('j');
-  const auto ev_type  = HAL_UARTEx_GetRxEventType( huart );
-  if( ev_type != HAL_UART_RXEVENT_TC ) {
-    return;
-  }
-  // HAL_UART_RXEVENT_TC    0   RxEvent linked to Transfer Complete event
-  // HAL_UART_RXEVENT_HT    1   RxEvent linked to Half Transfer event
-  // HAL_UART_RXEVENT_IDLE  2   RxEvent linked to IDLE event
-
-  auto u_isr = UART_LIDAR_LD20->USART_SR_REG;
-  __HAL_USART_CLEAR_IDLEFLAG( huart );
-
-  if( u_isr & USART_ISR_IDLE ) {
-    ++UVAR('l');
-  }
-  if( u_isr & USART_ISR_ORE ) {
-    ++UVAR('o');
-  }
-  if( n_szs < r_szs.size() ) {
-    r_val[n_szs  ] = ev_type;
-    r_szs[n_szs++] = sz;
+  unsigned sz = huart->RxXferSize;
+  if( sz > ubsz ) {
+    sz = ubsz;
   }
 
-  // if( sz != Lidar_LD20_Data::pkgSize ) {
-  //   return;
-  // }
-  r_sz = sz;
-
-  // TODO: correct mutex/flag
-  SCB_InvalidateDCache_by_Addr( ubuf, ubsz );
-  uint8_t *dst = (uint8_t*)(&lidar_pkg);
-  memset( dst,  '\0', Lidar_LD20_Data::pkgSize );
   memset( xbuf, '\0', ubsz );
-  std::memcpy( dst, ubuf, std::min(sz,(uint16_t)Lidar_LD20_Data::pkgSize) );
-  std::memcpy( xbuf, ubuf,std::min(sz,(uint16_t)ubsz) );
-  ++UVAR('k');
+  // SCB_InvalidateDCache_by_Addr( ubuf, ubsz );
+  // std::memcpy( dst, ubuf, std::min(sz,(uint16_t)Lidar_LD20_Data::pkgSize) );
+  std::memcpy( xbuf, ubuf, sz );
   memset( ubuf,  '\0', sizeof( ubuf ) );
 
-  leds.toggle( 4 );
+  leds.reset( 4 );
 }
+
+void HAL_UART_ErrorCallback( UART_HandleTypeDef *huart )
+{
+  ++UVAR('e');
+}
+
 
 int cmd_test0( int argc, const char * const * argv )
 {
   uint32_t t_step = UVAR('t');
   uint32_t n = arg2long_d( 1, argc, argv, UVAR('n'), 0 );
-  uint32_t abrt = arg2long_d( 2, argc, argv, 0, 0, 1 );
+  // uint32_t abrt = arg2long_d( 2, argc, argv, 0, 0, 1 );
   std_out << "# T1: n= " << n << NL;
 
   r_sz = 0;
   memset( ubuf,  0x00, sizeof( ubuf ) );
 
-  __HAL_USART_ENABLE( &huart_lidar );
-  if( abrt ) {
-    HAL_UART_Abort( &huart_lidar ); // test
-  }
 
-  // TODO: check ongouing
-  // TODO: try w/o ToIdle - idle state not detected (why?)
-  auto h_rc = HAL_UARTEx_ReceiveToIdle_DMA( &huart_lidar, (uint8_t*)&ubuf, sizeof(ubuf) );
-  // auto h_rc = HAL_UART_Receive_DMA( &huart_lidar, (uint8_t*)&ubuf, sizeof(ubuf) );
-  if( h_rc != HAL_OK ) {
-    std_out << "# Error: fail to RectToIdle: " << h_rc << NL;
-    // return 5;
+  std_out << "# gstate: " << huart_lidar.gState << " RxState: " << huart_lidar.RxState << NL;
+  if( huart_lidar.RxState == HAL_UART_STATE_READY ) {
+    __HAL_USART_ENABLE( &huart_lidar );
+    auto h_rc = HAL_UART_Receive_DMA( &huart_lidar, (uint8_t*)&ubuf, sizeof(ubuf) );
+    if( h_rc != HAL_OK ) {
+      std_out << "# Error: fail to RectToIdle: " << h_rc << NL;
+      // return 5;
+    }
   }
 
 
@@ -184,6 +161,7 @@ int cmd_test0( int argc, const char * const * argv )
 int cmd_abort( int argc, const char * const * argv )
 {
   HAL_UART_Abort( &huart_lidar );
+  __HAL_USART_DISABLE( &huart_lidar );
   return 0;
 }
 
