4e884b16d12e4bc1a4413bd0fdcb078b4198e045
Anton Guda <atu@nmetau.edu.ua>
2026-06-23 20:19:30 +0300

    Split oxc_sensor_base.h to oxc_coordtransform.h

---
+++ inc/oxc_coordtransform.h
@@ -0,0 +1,37 @@
+#ifndef _OXC_COORDTRANSFORM_H
+#define _OXC_COORDTRANSFORM_H
+
+#include <cmath>
+
+#include <oxc_types.h>
+
+using std::size_t;
+
+namespace oxc {
+
+struct CoordTransform {
+  public:
+   virtual ~CoordTransform() = default;
+   virtual float   toPhys( int32_t iv ) = 0;
+   virtual int32_t toInternal( float pv ) = 0;
+};
+
+struct LinearCoordTransform : public CoordTransform {
+  public:
+   struct PhysicalToInternalInit {};
+   //* default coeffs is internal to Physical, a!=0, ra!=0
+   constexpr LinearCoordTransform( float a_, float b_ )
+     : a( not_small(a_) ), b( b_ ), ra (1.0f/a) {}
+   //* coeffs for Physical to internal
+   constexpr LinearCoordTransform( float ra_, float rb_, PhysicalToInternalInit /*in*/)
+     :  a( 1.0f/not_small(ra_) ), b( -rb_ ), ra( not_small(ra_) ) {}
+   virtual float   toPhys( int32_t iv )  override  { return iv * a + b;  };
+   virtual int32_t toInternal( float pv ) override { return int32_t( ( ( pv - b ) * ra ) + 0.499f ); }
+   static constexpr float not_small( float aa ) { return std::fabsf(aa) > 1e-9f ? aa : 1.0f; }
+   float a, b, ra;
+};
+
+}; // namespace oxc;
+
+#endif
+
--- inc/oxc_sensor_base.h
+++ inc/oxc_sensor_base.h
@@ -4,32 +4,12 @@
 #include <cmath>
 
 #include <oxc_types.h>
+#include <oxc_coordtransform.h>
 
 using std::size_t;
 
 namespace oxc {
 
-struct CoordTransform {
-  public:
-   virtual ~CoordTransform() = default;
-   virtual float   toPhys( int32_t iv ) = 0;
-   virtual int32_t toInternal( float pv ) = 0;
-};
-
-struct LinearCoordTransform : public CoordTransform {
-  public:
-   struct PhysicalToInternalInit {};
-   //* default coeffs is internal to Physical, a!=0, ra!=0
-   constexpr LinearCoordTransform( float a_, float b_ )
-     : a( not_small(a_) ), b( b_ ), ra (1.0f/a) {}
-   //* coeffs for Physical to internal
-   constexpr LinearCoordTransform( float ra_, float rb_, PhysicalToInternalInit /*in*/)
-     :  a( 1.0f/not_small(ra_) ), b( -rb_ ), ra( not_small(ra_) ) {}
-   virtual float   toPhys( int32_t iv )  override  { return iv * a + b;  };
-   virtual int32_t toInternal( float pv ) override { return int32_t( ( ( pv - b ) * ra ) + 0.499f ); }
-   static constexpr float not_small( float aa ) { return std::fabsf(aa) > 1e-9f ? aa : 1.0f; }
-   float a, b, ra;
-};
 
 class PhysicalSensor {
   public:
