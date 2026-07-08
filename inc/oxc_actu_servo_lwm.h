8a1b55e4d894951526a2811ba711a8d80f1fd708
Anton Guda <atu@nmetau.edu.ua>
2026-07-08 15:32:52 +0300

    Update 87a_mov_servo_lwm

--- inc/oxc_actu_servo_lwm.h
+++ inc/oxc_actu_servo_lwm.h
@@ -2,26 +2,25 @@
 #define _OXC_ACTU_SERVO_LWM_H
 
 #include <oxc_actu_servo_lwm_base.h>
+#include <oxc_coordtransform.h>
 
 namespace oxc {
 
 
 // standard LWM controlled servo
-class ActuServoLWM : public ActuServoLWMBase {
+class ActuServoLWM : public ActuServoLWMBase, public ActuPositionSink {
   public:
-   ActuServoLWM( PwmCtl &pwmc_, std::size_t ch_ )
-     : ActuServoLWMBase( pwmc_, ch_ )
-   {
-     props = canSetX | canBreak;
-   }
-   virtual ReturnCode setX(  float x  ) override { return setXV( x ); }
-   virtual ReturnCode setV(  float v  ) override { return rcErr; }
-   virtual ReturnCode setTo( float to ) override { return rcErr; }
-   virtual ReturnCode stop() override { pwmc.setPwm( ch, 0 ); return rcOk; }
-   virtual ReturnCode brk()  override { return rcOk; };
-   virtual ReturnCode idle() override { return stop(); };
-
+   ActuServoLWM( RoboPwmCtl &pwmc_, std::size_t ch_, CoordTransform &coo_tr_ )
+     : ActuServoLWMBase( pwmc_, ch_ ), coo_tr( coo_tr_ ) {}
+   virtual ReturnCode setQ( float q ) override {
+     q_int = std::clamp( coo_tr.toInternal( q ), -1.0f, 1.0f );
+     q_phy = coo_tr.toPhys( q_int );
+     return setXV( q_int );
+   };
+   virtual ReturnCode brk()           override { return rcOk; };
+   virtual ReturnCode idle()          override { return pwmc.setPulse( ch, 0 ); };
   protected:
+   CoordTransform &coo_tr;
 
 };
 
--- inc/oxc_actu_servo_lwm_base.h
+++ inc/oxc_actu_servo_lwm_base.h
@@ -1,25 +1,18 @@
 #ifndef _OXC_ACTU_SERVO_LWM_BASE_H
 #define _OXC_ACTU_SERVO_LWM_BASE_H
 
-#include <oxc_actu_base.h>
-#include <oxc_pwmctl.h>
+#include <oxc_robo_base.h>
+#include <oxc_robopwmctl.h>
 
 namespace oxc {
 
 
-// base: LWM controlled servo
-class ActuServoLWMBase : public Actuator {
+// base: LWM controlled servo: not Robo*, just common code
+class ActuServoLWMBase {
   public:
-   ActuServoLWMBase( PwmCtl &pwmc_, std::size_t ch_ )
+   ActuServoLWMBase( RoboPwmCtl &pwmc_, std::size_t ch_ )
      : pwmc( pwmc_ ), ch(ch_)   {  }
-   ReturnCode setXV( float xv ); // base: x or v
-   // all other unimplemented
-   // virtual ReturnCode set_v( float v ) override;
-   // virtual ReturnCode set_x( float x ) override;
-   // virtual ReturnCode stop() override;
-   // virtual ReturnCode brk() override;
-   // virtual ReturnCode idle() override;
-   virtual ReturnCode init() { return rcOk; /* pwmc.initHW() ? rcOk : rcErr; */ } // TODO: fix
+   ReturnCode setXV( float xv ); // base: x or v [-1:1]
 
    void set_t_on_limits( uint32_t t_on_min_, uint32_t t_on_max_ )
    {
@@ -29,7 +22,7 @@ class ActuServoLWMBase : public Actuator {
      t_on_dlt = ( t_on_max - t_on_min ) / 2; // /2 as x: +-1
    };
   protected:
-   PwmCtl &pwmc;
+   RoboPwmCtl &pwmc;
    std::size_t ch;
    uint32_t t_on_min {  500 }; // in us
    uint32_t t_on_max { 2500 };
