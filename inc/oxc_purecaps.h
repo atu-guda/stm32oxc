da45178e3e5c24f09385ca97f68d3c69cee974f3
Anton Guda <atu@nmetau.edu.ua>
2026-09-01 15:34:54 +0300

    Rework PwmPureCapability

--- inc/oxc_purecaps.h
+++ inc/oxc_purecaps.h
@@ -35,13 +35,39 @@ class PinsPureCapability {
 
 
 //* frequiency in Hz, duty: [0:1]
+//* if freq is changed, drop duty0, shifts to 0
 class PwmPureCapability {
   public:
-   virtual ReturnCode setDuty(  size_t ch, float duty ) noexcept = 0;
-   virtual ReturnCode setPulse( size_t ch, float pu_s ) noexcept = 0;
-   virtual ReturnCode setShift( size_t ch, float sh_s ) noexcept = 0;
-   virtual ReturnCode setFreq( float freq )             noexcept = 0;
-   virtual float getFreq() const                        noexcept = 0;
+   enum { max_cfg_reg = 8 };
+   ReturnCode setDuty(  size_t ch, float duty ) noexcept { return setDutyRaw(  ch,  duty2raw(duty) ); }
+   ReturnCode setPulse( size_t ch, float pu_s ) noexcept { return setDutyRaw(  ch, pulse2raw(pu_s) ); }
+   ReturnCode setShift( size_t ch, float sh_s ) noexcept { return setShiftRaw( ch, shift2raw(sh_s) ); }
+   ReturnCode setFreq( float freq ) noexcept {
+     uint32_t cfgs[max_cfg_reg];
+     ReturnCode rc = freq2cfgs( freq, cfgs );
+     if( rc.isError() ) { return rc; }
+     return applyCfg( cfgs );
+   }
+   float_er getFreq() const noexcept {
+     uint32_t cfgs[max_cfg_reg];
+     ReturnCode rc = storeCfg( cfgs );
+     if( rc.isError() ) { return std::unexpected(rc); }
+     return cfg2freq( cfgs );
+   };
+   // for commonify usage + ll
+   virtual uint32_t duty2raw(  float duty ) const noexcept = 0;
+   virtual uint32_t pulse2raw( float pu_s ) const noexcept = 0;
+   virtual uint32_t shift2raw( float pu_s ) const noexcept = 0;
+   virtual ReturnCode freq2cfgs( float freq, std::span<uint32_t> cfgs ) const noexcept = 0;
+   virtual float_er  cfg2freq( std::span<const uint32_t> cfgs ) const noexcept = 0;
+   // low-level interface
+   virtual ReturnCode setDutyRaw(  size_t ch, int32_t dr ) noexcept = 0;
+   virtual ReturnCode setShiftRaw( size_t ch, int32_t sr ) noexcept = 0;
+   virtual int32_t_er getDutyRaw(  size_t ch )  noexcept = 0;
+   virtual int32_t_er getShiftRaw( size_t ch )  noexcept = 0;
+   virtual ReturnCode applyCfg( std::span<const uint32_t> cfgs )  noexcept = 0;
+   virtual ReturnCode storeCfg( std::span<      uint32_t> cfgs ) const noexcept = 0;
+  protected:
 };
 
 
